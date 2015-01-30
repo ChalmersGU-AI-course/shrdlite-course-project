73f7a616a7155d9ce5ca60ec9f94c2fb9e951e7d
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2015-01-30 16:43:35 +0100

    Possible to change world in the browser

--- shrdlite-html.ts
+++ shrdlite-html.ts
@@ -3,10 +3,46 @@
 ///<reference path="ExampleWorlds.ts"/>
 ///<reference path="lib/jquery.d.ts" />
 
+var defaultWorld = 'small';
+var useSpeech = true;
+
 $(function(){
-    var startWorld = 'small';
-    var useSpeech = false;
-    var world = new SVGWorld(ExampleWorlds[startWorld], useSpeech);
+    $('#exampleworlds').empty();
+    for (var wname in ExampleWorlds) {
+        $('<input type="submit">').val(wname)
+            .click(changeCurrentWorld)
+            .appendTo($('#exampleworlds'));
+    }
+
+    var name = getURLParameter('world');
+    if (!(name in ExampleWorlds)) {
+        name = defaultWorld;
+    }
+
+    var world = new SVGWorld(ExampleWorlds[name], useSpeech);
     Shrdlite.interactive(world);
 });
 
+
+function changeCurrentWorld() : void {
+    var name = this.value;
+    if (confirm("Do you want to reset to the " + name + " world?")) {
+        var url = window.location.href.split('?')[0];
+        window.location.href = url + '?world=' + name;
+    }
+}
+
+
+// Borrowed from:
+// http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html
+
+function getURLParameter(sParam) : string {
+    var sPageURL = window.location.search.slice(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+}​
