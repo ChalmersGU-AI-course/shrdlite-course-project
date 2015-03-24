cf782ff1fa35cca2ec720a35cb1873d3dd90b6fc
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2015-03-24 13:30:46 +0100

    print out query as debugging

--- server/Main.hs
+++ server/Main.hs
@@ -3,12 +3,15 @@
 
 module Main where
 
-import Web.Scotty
+import Control.Monad.IO.Class (liftIO)
 import Network.Wai.Middleware.Static
+import Web.Scotty
 
 main :: IO ()
 main = scotty 8000 (do middleware (staticPolicy (addBase "..")) -- serv all shrdlite files
                        get "/ajax" ajaxing) -- use /ajax as the entrypoint for all interesting stuff.
 
 ajaxing :: ActionM ()
-ajaxing = html "Hello"
+ajaxing = do ps <- params
+             liftIO (print ps)
+             html "Hello"
