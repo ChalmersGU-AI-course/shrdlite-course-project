5138ac0c2f9fa062c5dac3f05a9f66a2a3f07fcf
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2015-03-24 16:21:39 +0100

    more docs

--- server/Types/Parser.hs
+++ server/Types/Parser.hs
@@ -9,18 +9,18 @@ data Result =
 
 data Command =
   Command {c_cmd :: String -- ^ move, pick up, etc
-          ,c_ent :: Maybe Entity
-          ,c_loc :: Maybe Location
+          ,c_ent :: Maybe Entity -- ^ with what
+          ,c_loc :: Maybe Location -- ^ to where
           }
 
 data Entity =
-  Entity {e_quant::String -- ^ a quantity
+  Entity {e_quant::String -- ^ quantifier, the, any ...
          ,e_obj :: Object -- ^ an object
          }
 
 data Location =
-  Location {l_rel::String
-           ,l_ent::Entity
+  Location {l_rel::String -- ^ ontop, beside, inside ...
+           ,l_ent::Entity -- ^ in relation to what
            }
 
 -- | An object is either located in relation to some other object or
@@ -38,6 +38,7 @@ data Object
 
 
 -- "put the black ball in a box on the floor"
+-- -> move the black ball inside any box that is on top of the floor
 example :: Command
 example =
   Command "move"
