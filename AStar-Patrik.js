ea000cbd037ab7eb1045602b3178aa66f27a9f65
Patrik Nygren <patriknygren82@gmail.com>
2015-03-25 18:01:17 +0100

    Just playing around with a Astar implementation idea

---
+++ AStar-Patrik.js
@@ -0,0 +1,6 @@
+///<reference path="lib/node.d.ts"/>
+///<reference path="World.ts"/>
+var PQ = require('libstl').PriorityQueue;
+function genericSearch(graph, start, goal) {
+}
+exports.genericSearch = genericSearch;
