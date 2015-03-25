72e86f5abc6be980ef5f02d1c1c0c697e759cfe7
axhav <william_axhav@hotmail.com>
2015-03-25 13:19:48 +0100

    start astar code continued

--- AStar.js
+++ AStar.js
@@ -1,16 +1,17 @@
 /// <reference path="Datastructures/PriorityQueue.ts"/>
 var AStar;
 (function (AStar) {
-    function asdasd() {
-        var PriorityQueue = require('./libstl').PriorityQueue;
+    var PriorityQueue = require('PriorityQueue');
+    function asdasdJarnaMain() {
         var queue = new PriorityQueue();
+        var came_from = {};
     }
-    AStar.asdasd = asdasd;
+    AStar.asdasdJarnaMain = asdasdJarnaMain;
     function reconstruct_path(came_from, current) {
         var total_path = [];
         while (current in came_from) {
             current = came_from;
-            total_path = total_path[current];
+            total_path.push(current);
         }
         return total_path;
     }
