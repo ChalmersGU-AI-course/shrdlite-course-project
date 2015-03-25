b8e78fad2f81fbac3ac289c08ae23495332d7979
Gustav Mörtberg <gustavmortberg@gmail.com>
2015-03-25 16:19:04 +0100

    Starting implementing astar

--- lib/astar.ts
+++ lib/astar.ts
@@ -1,1 +1,36 @@
+/// <reference path="collections.ts" />
+
 // A-Star A-team implementation by Team Dandelion
+
+module aStar {
+	export function aStar(fromState : WorldState, toState : WorldState) : int {
+		var evaluatedStates = new collections.Set<WorldState>(); 
+		var statesToEvalute = new collections.PriorityQueue<WorldState>();
+		var pathToState = new collections.Map<WorldState, WorldState>();
+		var costToState : int[];
+		var optimalCost : int = getDistance(fromState, toState);
+
+		costToState[0] = 0;
+		statesToEvalute.add(fromState);
+
+		while(!statesToEvalute.isEmpty()) {
+			currentState = statesToEvalute.dequeue();
+			if(currentState == toState) {
+
+			}
+		}
+		// (Check that worlds are compatible.)
+
+		// Populate set with states reachable from from-state.
+		// 
+
+		// Do search with heuristic
+
+		// Return path found with search
+	}
+
+	// Takes two worldstates and returns the abstract distance between them.
+	function getDistance(from: WorldState, to: WorldState) : int {
+		return 1;
+	}
+}
