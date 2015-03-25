a97c29c9addb48de781e83eddc2867435174b1be
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2015-03-25 12:51:43 +0100

    documentation for astar

--- server/AStar.hs
+++ server/AStar.hs
@@ -2,10 +2,15 @@
 
 module AStar where
 
-astar :: (Ord measure, Ord node)
+
+-- | A quite general A* search function, we represent the graph as a
+-- function from nodes to neighbours and any distances as a function
+-- of two neighbours, thus the user can choose whatever graph
+-- representation they want.
+astar :: (Ord measure)
       => (node -> [node]) -- ^ function from node to neighbours
       -> (node -> node -> measure) -- ^ distance between neighbours
-      -> (node -> measure) -- ^ distance heuristic for goal
+      -> (node -> measure) -- ^ distance heuristic to goal
       -> (node -> Bool) -- ^ goal node predicate
       -> node -- ^ start node
       -> [node] -- ^ path from start to goal (can be empty if no path is found)
