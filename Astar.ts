eea86e21a5510d9213358d64374fefc44a46f496
Fredrik <fredrik.mikael.strom@gmail.com>
2015-03-26 15:26:40 +0100

    started with AStar algo, not finnished

--- Astar.ts
+++ Astar.ts
@@ -6,6 +6,27 @@ module Astar {
         //TODO
         return 0;
     }
+
+    export function getMinFScore(fscore : number[]){
+        var result : number;
+        result=fscore[0];
+        var index : number = 0;
+        var indexout : number = 0;
+        fscore.forEach(fs => {
+            if(fs < result){
+                result = fs;
+                indexout = index;
+            }
+            index ++;}
+        );
+        return indexout;    
+    }
+
+    export function reconstruct_path(came_from : number[], goal:number):number[]{
+        var result_path:number[];
+        //TODO
+        return result_path;
+    }
     
     export function star (start: number, goal : number): number[]{
         var closedset : number [];   // The set of nodes already evaluated.
@@ -19,6 +40,34 @@ module Astar {
         g_score[start] = 0;
         
         f_score[start] = g_score[start] + heuristic_cost_estimate(start, goal);
+        
+        while (openset.length){
+            current = getMinFScore(openset);
+            if(current === goal)
+                return reconstruct_path(came_from, goal);
+            
+            
+        }
+        
+ 
+        remove current from openset
+        add current to closedset
+        for each neighbor in neighbor_nodes(current)
+            if neighbor in closedset
+                continue
+            tentative_g_score := g_score[current] + dist_between(current,neighbor)
+ 
+            if neighbor not in openset or tentative_g_score < g_score[neighbor] 
+                came_from[neighbor] := current
+                g_score[neighbor] := tentative_g_score
+                f_score[neighbor] := g_score[neighbor] + heuristic_cost_estimate(neighbor, goal)
+                if neighbor not in openset
+                    add neighbor to openset
+ 
+        return failure
+        
+        
+        
         return result; 
     }
 }
