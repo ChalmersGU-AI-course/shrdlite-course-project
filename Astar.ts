4979fae0516df567ff95e3764a89dd8907dc0e1d
Fredrik <fredrik.mikael.strom@gmail.com>
2015-03-26 15:06:01 +0100

    starting with AStar, declarations of variables and functions

--- Astar.ts
+++ Astar.ts
@@ -1,3 +1,24 @@
 module Astar {
+    
 
+    export function heuristic_cost_estimate(current : number, goal : number) : number{
+        
+        //TODO
+        return 0;
+    }
+    
+    export function star (start: number, goal : number): number[]{
+        var closedset : number [];   // The set of nodes already evaluated.
+        var openset : number [];
+        openset[0] = start;       // The set of tentative nodes to be evaluated, initially containing the start node
+        var came_from : number [];    // The map of navigated nodes.
+        var result : number [];
+        
+        var g_score : number [];
+        var f_score : number [];
+        g_score[start] = 0;
+        
+        f_score[start] = g_score[start] + heuristic_cost_estimate(start, goal);
+        return result; 
+    }
 }
