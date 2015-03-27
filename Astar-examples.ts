38fe46a94807487b17ff3b90f5aca1f00780b89d
Fredrik <fredrik.mikael.strom@gmail.com>
2015-03-27 16:44:18 +0100

    added Astar-examples, not finnished

---
+++ Astar-examples.ts
@@ -0,0 +1,97 @@
+class Astar-examples {
+    constructor();
+
+    
+}
+
+interface neighbors<T>{
+    getneighbors(instate: T):Array<T>;
+}
+
+public interface graph<T> extends neighbors{
+    private _nodes : Array<Array<T>>;
+    private _edges : number[][];
+        
+    get nodes(): Array<Array<T>>{
+        return this._nodes;
+    }
+    
+    set nodes(n: Array<Array<T>>) {
+        this._edges = n;
+    }
+
+    get edges(): number[][]{
+        return this._edges;
+    }
+    
+    set edges(e: number[][]) {
+        this._edges = e;
+    }
+}
+
+public class eightpuzzle implements  graph<number[][]>, neighbors<number[][]>{
+    initstate : number[][];
+    goalstate : number[][];
+    
+    constructor(){
+        this.initstate = createState([2, 4, 0, 5, 7, 1 ,8 ,3, 6]);
+        this.goalstate = createState([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+        this.nodes = allstates();
+    }
+    
+    findZero(instate: number[][]):number[]{
+        var indexzero : number[] = [-1,-1];
+        for(var i = 0; i < 4; i ++){
+            if(instate[i].indexOf(0) >= 0){
+                indexzero[0] = i;
+                indexzero[1] = instate[i].indexOf(0);
+                return indexzero;
+            }
+        }
+        return indexzero;
+    }
+    
+    getneighbors(instate: number[][]):Array<number [][]>{
+        var result: Array<number[][]>;
+        var indexzero = findZero(instate);
+        
+        
+        var nindexzero = indexzero;
+        if(indexzero[0]-1 >= 0){
+            nindexzero[0] = indexzero[0]-1;
+            result.push(transitionState(instate, indexzero, nindexzero));
+        }
+        if(indexzero[0]+1 < 3){
+            nindexzero[0] = indexzero[0]+1;
+            result.push(transitionState(instate, indexzero, nindexzero));
+        }
+        if(indexzero[1]-1 >= 0){
+            nindexzero[1] = indexzero[1]-1;
+            result.push(transitionState(instate, indexzero, nindexzero));    
+        }
+        if(indexzero[1]+1 < 3){
+            nindexzero[1] = indexzero[1]+1;
+            result.push(transitionState(instate, indexzero, nindexzero));    
+        }
+        return result;    
+    }
+    
+    transitionState(instate: number[][], oldZ: number[],newZ: number[]): number[][]{
+        instate[oldZ[0]][oldZ[1]] = instate[newZ[0]][newZ[1]];
+        instate[newZ[0]][newZ[1]] = 0;
+        return instate;
+    }
+    
+    createState(input : number[]):number[]{
+        var sum : number = 0;
+        var nstate : number [][];
+        for(var i = 0; i < 3; i++){
+            for(var j = 0; j < 3; j++){
+                this.nstate[i][j] = input[sum];
+                sum ++;
+                j++;
+            }
+        }    
+        return nstate;
+    }
+}
