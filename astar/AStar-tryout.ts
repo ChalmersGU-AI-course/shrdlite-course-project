4c46ae489f815a8e8b0820a4bc8ad577723ed0ce
Patrik Nygren <patriknygren82@gmail.com>
2015-03-28 00:03:33 +0100

    AStar-tryout and test finished (working example)

--- astar/AStar-tryout.ts
+++ astar/AStar-tryout.ts
@@ -1,17 +1,29 @@
 ///<reference path="../lib/node.d.ts"/>
 ///<reference path="collections.d.ts"/>
 
-// external module
 import C = require('./collections');
 
-export module AS {
+export module AS { // AStar
+
+  export class ANodeDict<T extends Heuristic> {
+    dict: C.collections.Dictionary<string, ANode<T>>;
+    set(name: string, node: ANode<T>): ANode<T> {
+      return this.dict.setValue(name, node);
+    }
+    get(name: string): ANode<T> {
+      return this.dict.getValue(name);
+    }
+    constructor() {
+      this.dict = new C.collections.Dictionary<string, ANode<T>>();
+    }
+  }
 
   export class ANode<T extends Heuristic> {
     state: T;
-    prev: ANode<T>;   // just one node enables a walk back to start to return the path.
-    next: ANode<T>[]; // list of possible nodes to walk to
+    prev: string;   // just one node enables a walk back to start to return the path.
+    next: string[]; // list of possible nodes to walk to
     cost: number;
-    constructor(state: T, prev?: ANode<T>, next?: ANode<T>[], cost=0) {
+    constructor(state: T, prev: string, next: string[], cost=0) {
       this.state = state;
       this.prev = prev;
       this.next = next;
@@ -26,7 +38,6 @@ export module AS {
 
   /*
    * AStar simple implementation. Requires an implementation of Heuristic and ANode.
-   * TODO: Put a limit on search for termination
    * TODO: Cycle checking: Keep record of visited nodes for termination and dont
    * expand (add to PQ if they are already visited)
    * TODO: Multiple path pruning: Keep redord of paths to visitied nodes and their
@@ -35,71 +46,38 @@ export module AS {
    * then, A* with multiple path pruning always finds the shortest path to a goal.
    */
   export function search<T extends Heuristic>(start: ANode<T>,
-                                              goal: T): T[] {
-    // var comp = new Compare(goal);
+                                              goal: T,
+                                              dict: ANodeDict<T>): T[] {
     var frontier = new C.collections.PriorityQueue<ANode<T>>(compClosure(goal));
     frontier.enqueue(start);
 
     while(!frontier.isEmpty()) {
       var n : ANode<T> = frontier.dequeue();
-      if(n) {
-        if(n.state.match(goal)) { /// how should this be implemented?
-          return path<T>(n);
+      if(n) {                     // check if node is undefined
+        if(n.state.match(goal)) {
+          return path<T>(n, dict);
         }
-        var neighbours = n.next;
-        for(var i=0; i<neighbours.length; i++) {
-          var _neighbour = neighbours[i];
+        for(var i=0; i<n.next.length; i++) { // for all neighbours
+          var _neighbour = dict.get(n.next[i]);
           frontier.enqueue(_neighbour);
         }
       } else {
         throw "Node undefined";
       }
     }
-    // return [start.state];
   };
 
 
   //////////////////////////////////////////////////////////////////////
   // private classes and functions
 
-  /*
-   * Class for wrapping the comparing function used by PQ
-   */
-  // class Compare<T extends Heuristic> {
-  //   goal: T;
-  //   constructor(goal: T) {
-  //     this.goal = goal;
-  //   }
-  //   /*
-  //    * Comparing function
-  //    */
-  //   compare(a: Node<T>, b: Node<T>): number {
-  //     var aCost = this.fcost(a, this.goal);
-  //     var bCost = this.fcost(b, this.goal);
-  //     var comp;
-  //     if (aCost > bCost)
-  //       comp = 1;
-  //     else if(aCost === bCost)
-  //       comp = 0;
-  //     else
-  //       comp = -1;
-  //     return comp
-  //   }
-  //   /*
-  //    * Adds cost function and heuristic function for PQ to work properly
-  //    */
-  //   fcost(current: Node<T>, goal: T): number {
-  //     return current.cost + current.state.heuristic(goal);
-  //   }
-  // }
-
   function compClosure<T extends Heuristic>(goal: T) {
     /*
      * Comparing function
      */
     function compare<T extends Heuristic>(a: ANode<T>, b: ANode<T>): number {
-      var aCost = this.fcost(a, null);
-      var bCost = this.fcost(b, null);
+      var aCost = a.cost + a.state.heuristic(goal);
+      var bCost = b.cost + b.state.heuristic(goal);
       var res;
       if (aCost < bCost)
         res = 1;
@@ -113,21 +91,15 @@ export module AS {
   }
 
   /*
-   * Adds cost function and heuristic function for PQ to work properly
-   */
-  function fcost<T extends Heuristic>(current: ANode<T>, goal: T): number {
-    return current.cost + current.state.heuristic(goal);
-  }
-
-  /*
    * extracts the path from the ANode back to start node
    */
-  function path<T extends Heuristic>(n: ANode<T>) : T[] {
+  function path<T extends Heuristic>(n: ANode<T>,
+                                     dict: ANodeDict<T>): T[] {
     var _path: T[] = [];
-    var n: ANode<T> = n;
-    while(n != null) {
-      _path.push(n.state)
-      n = n.prev;
+    var _n: ANode<T> = n;
+    while(_n != null) {
+      _path.push(_n.state)
+      _n = dict.get(_n.prev);
     }
     return _path.reverse();
   }
--- test/AStar-tryout-test.ts
+++ test/AStar-tryout-test.ts
@@ -1,76 +1,83 @@
 ///<reference path="../lib/node.d.ts"/>
 ///<reference path="../typings/mocha/mocha.d.ts" />
 ///<reference path="../typings/chai/chai.d.ts" />
-///<reference path="../AStar-tryout.ts"/>
 
 import chai = require('chai');
-import ASt = require('../AStar-tryout');
+import A = require('../astar/AStar-tryout');
 
-var expect = chai.expect;
+module AStarTest {
 
-class City implements ASt.Heuristic {
-  name: string;
-  h: number;
-  heuristic(goal: City) { // ignores the argument
-    return this.h;
+  class City implements A.AS.Heuristic {
+    name: string;
+    h: number;
+    match(goal: City) {
+      return this.name === goal.name;
+    }
+    heuristic(goal: City) { // ignores the argument
+      return this.h;
+    }
+    constructor(name: string, h: number) {
+      this.name = name;
+      this.h = h;
+    }
   }
-  constructor(name: string, h: number) {
-    this.name = name;
-    this.h = h;
-  }
-}
 
-// Cities
-var arad      = new City( "Arad",      366 );
-var sibiu     = new City( "Sibiu",     253 );
-var timisoara = new City( "Timisoara", 329 );
-var zerind    = new City( "Zerind",    374 );
-var fagaras   = new City( "Fagaras",   176 );
-var oradea    = new City( "Oradea",    380 );
-var rimnicu   = new City( "Rimnicu",   193 );
-var craiova   = new City( "Craiova",   160 );
-var pitesti   = new City( "Pitesti",   100 );
-var bucharest = new City( "Bucharest", 0   );
+  // Cities
+  var arad      = new City( "Arad",      366 );
+  var sibiu     = new City( "Sibiu",     253 );
+  var timisoara = new City( "Timisoara", 329 );
+  var zerind    = new City( "Zerind",    374 );
+  var fagaras   = new City( "Fagaras",   176 );
+  var oradea    = new City( "Oradea",    380 );
+  var rimnicu   = new City( "Rimnicu",   193 );
+  var craiova   = new City( "Craiova",   160 );
+  var pitesti   = new City( "Pitesti",   100 );
+  var bucharest = new City( "Bucharest", 0   );
 
-// Romainia map (Start point Arad)
-var null_arad         = new ASt.Node<City>(arad, null, [arad_sibiu, arad_timisoara, arad_zerind], 0);
+  // Romainia map (Start point Arad)
 
-var arad_sibiu        = new ASt.Node<City>(sibiu, null_arad, [sibiu_arad, sibiu_fagaras, sibiu_oradea, sibiu_rimnicu], 140);
-var arad_timisoara    = new ASt.Node<City>(timisoara, null_arad, [], 118);
-var arad_zerind       = new ASt.Node<City>(zerind, null_arad, [], 75);
+  var dict = new A.AS.ANodeDict<City>();
 
-var sibiu_arad        = new ASt.Node<City>(arad, arad_sibiu, [], 280);
-var sibiu_fagaras     = new ASt.Node<City>(fagaras, arad_sibiu, [fagaras_sibiu, fagaras_bucharest], 239); // list
-var sibiu_oradea      = new ASt.Node<City>(oradea, arad_sibiu, [], 291);
-var sibiu_rimnicu     = new ASt.Node<City>(rimnicu, arad_sibiu, [rimnicu_craiova, rimnicu_pitesti, rimnicu_sibiu], 220);
+  dict.set("pitesti_bucharest",  new A.AS.ANode<City>(bucharest, "rimnicu_pitesti", [], 418));
+  dict.set("pitesti_craiova",    new A.AS.ANode<City>(craiova, "rimnicu_pitesti", [], 455));
+  dict.set("pitesti_rimnicu",    new A.AS.ANode<City>(rimnicu, "rimnicu_pitesti", [], 414));
 
-var fagaras_sibiu     = new ASt.Node<City>(sibiu, sibiu_fagaras, [], 338);
-var fagaras_bucharest = new ASt.Node<City>(bucharest, sibiu_fagaras, [], 450);
+  dict.set("rimnicu_craiova",    new A.AS.ANode<City>(craiova, "sibiu_rimnicu", [], 366));
+  dict.set("rimnicu_pitesti",    new A.AS.ANode<City>(pitesti, "sibiu_rimnicu", ["pitesti_bucharest", "pitesti_craiova", "pitesti_rimnicu"], 317));
+  dict.set("rimnicu_sibiu",      new A.AS.ANode<City>(sibiu, "sibiu_rimnicu", [], 300));
 
-var rimnicu_craiova   = new ASt.Node<City>(craiova, sibiu_rimnicu, [], 366);
-var rimnicu_pitesti   = new ASt.Node<City>(pitesti, sibiu_rimnicu, [pitesti_bucharest, pitesti_craiova, pitesti_rimnicu], 317);
-var rimnicu_sibiu     = new ASt.Node<City>(sibiu, sibiu_rimnicu, [], 300);
+  dict.set("fagaras_sibiu",      new A.AS.ANode<City>(sibiu, "sibiu_fagaras", [], 338));
+  dict.set("fagaras_bucharest",  new A.AS.ANode<City>(bucharest, "sibiu_fagaras", [], 450));
 
-var pitesti_bucharest = new ASt.Node<City>(bucharest, rimnicu_pitesti, [], 418);
-var pitesti_craiova   = new ASt.Node<City>(craiova, rimnicu_pitesti, [], 455);
-var pitesti_rimnicu   = new ASt.Node<City>(rimnicu, rimnicu_pitesti, [], 414);
+  dict.set("sibiu_arad",         new A.AS.ANode<City>(arad, "arad_sibiu", [], 280));
+  dict.set("sibiu_fagaras",      new A.AS.ANode<City>(fagaras, "arad_sibiu", ["fagaras_sibiu", "fagaras_bucharest"], 239));
+  dict.set("sibiu_oradea",       new A.AS.ANode<City>(oradea, "arad_sibiu", [], 291));
+  dict.set("sibiu_rimnicu",      new A.AS.ANode<City>(rimnicu, "arad_sibiu", ["rimnicu_craiova", "rimnicu_pitesti", "rimnicu_sibiu"], 220));
+
+  dict.set("arad_sibiu",         new A.AS.ANode<City>(sibiu, "arad", ["sibiu_arad", "sibiu_fagaras", "sibiu_oradea", "sibiu_rimnicu"], 140));
+  dict.set("arad_timisoara",     new A.AS.ANode<City>(timisoara, "arad", [], 118));
+  dict.set("arad_zerind",        new A.AS.ANode<City>(zerind, "arad", [], 75));
+
+  dict.set("arad",               new A.AS.ANode<City>(arad, null, ["arad_sibiu", "arad_timisoara", "arad_zerind"], 0));
 
 
-describe('AStar', () => {
-  var city;
+  var expect = chai.expect;
 
-  before(function () {
-    city = arad;
-  });
-
-  describe('heuristic', () => {
-    it('should return the h value of the city', (done) => {
-      expect(city.heuristic(null)).to.equals(366);
-      done();
+  describe('AStar', () => {
+    describe('heuristic', () => {
+      it('should return the correct heuristic value of the city', (done) => {
+        expect(arad.heuristic(null)).to.equals(366);
+        done();
+      });
+      it('should return a path to Bucharest', (done) => {
+        var arad = dict.get("arad");
+        var path = A.AS.search(arad, bucharest, dict);
+        expect(path[path.length-1].name).to.equals("Bucharest");
+        done();
+      });
     });
   });
 
-});
-
+}
 
 
