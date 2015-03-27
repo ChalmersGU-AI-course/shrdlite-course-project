a6abf902db678c98db1330f5e7133e008fa02f46
linus börjesson <linus.bor@gmail.com>
2015-03-27 15:26:46 +0100

    Fixed some import statements.

--- search/Search.ts
+++ search/Search.ts
@@ -1,9 +1,9 @@
 ///<reference path="./Graph.ts" />
 
-import G=Graph.Graph
-import N=Graph.Node
-
 module Search {
+  import G = Graph.Graph;
+  import N = Graph.Node;
+
   export interface Search<Id, Cost, Path> {
     search(graph: G<Id, Cost>, start: N<Id>, end: N<Id>): Path
   }
