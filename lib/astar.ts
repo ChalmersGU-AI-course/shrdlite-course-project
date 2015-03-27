bd90b9b82f4c966f05d81b641c6f33731f928438
Niklas Wärvik <niklas.warvik@gmail.com>
2015-03-27 16:15:41 +0100

    changed tabs to spaces

--- lib/astar.ts
+++ lib/astar.ts
@@ -4,53 +4,55 @@
 // A-Star A-team implementation by Team Dandelion
 
 module aStar {
-	export function aStar(fromState : WorldState, toState : WorldState) : number {
-		var evaluatedStates = new collections.Set<WorldState>(); 
-		var statesToEvalute = new collections.PriorityQueue<WorldState>(compareStates);
-		var pathToState = new collections.Dictionary<WorldState, WorldState>();
-		var g_score = new collections.Dictionary<WorldState, number>();
-		var f_score = new collections.Dictionary<WorldState, number>();
-		//var costToState : int[];
-		//var optimalCost : int = getDistance(fromState, toState);
-		//costToState[0] = 0;
-		statesToEvalute.add(fromState);
-		g_score.setValue(fromState, 0);
-		f_score.setValue(fromState, g_score.getValue(fromState) + getDistance(fromState, toState));
+    export function aStar(fromState : WorldState, toState : WorldState) : number {
+        var evaluatedStates = new collections.Set<WorldState>(); 
+        var statesToEvalute = new collections.PriorityQueue<WorldState>(compareStates);
+        var pathToState = new collections.Dictionary<WorldState, WorldState>();
+        var g_score = new collections.Dictionary<WorldState, number>();
+        var f_score = new collections.Dictionary<WorldState, number>();
+        //var costToState : int[];
+        //var optimalCost : int = getDistance(fromState, toState);
+        //costToState[0] = 0;
+        statesToEvalute.add(fromState);
+        g_score.setValue(fromState, 0);
+        f_score.setValue(fromState, g_score.getValue(fromState) + getDistance(fromState, toState));
 
 
-		while(!statesToEvalute.isEmpty()) {
-			var currentState = statesToEvalute.dequeue();
-			if(currentState == toState) {
+        while(!statesToEvalute.isEmpty()) {
+            var currentState = statesToEvalute.dequeue();
+            if(currentState == toState) {
 
-			}
-		}
-		// (Check that worlds are compatible.)
+            }
+        }
+        // (Check that worlds are compatible.)
 
-		// Populate set with states reachable from from-state.
-		// 
+        // Populate set with states reachable from from-state.
+        // 
 
-		// Do search with heuristic
+        // Do search with heuristic
 
-		// Return path found with search
-		return -1;
-	}
+        // Return path found with search
+        return -1;
+    }
 
-	// Takes two worldstates and returns the abstract distance between them.
-	function getDistance(from: WorldState, to: WorldState) : number {
-		return 1;
-	}
+    // Takes two worldstates and returns the abstract distance between them.
+    function getDistance(from: WorldState, to: WorldState) : number {
+        return 1;
+    }
 
-	function compareStates(a : WorldState, b : WorldState) : number {
-		return 1;
-	}
+    function compareStates(a : WorldState, b : WorldState) : number {
+        return 1;
+    }
 
-	interface ExtendedWorldState extends World.WorldState {
-		costToState : int;
-		pathToState : WorldState[];
-		abstractDist : int;
+    interface ExtendedWorldState extends World.WorldState {
+        costToState : int;
+        pathToState : WorldState[];
 
-		function int compareTo(first : ExtendedWorldState, second : ExtendedWorldState) {
-			return 1;
-		}
-	}
+
+        abstractDist : int;
+
+        function int compareTo(first : ExtendedWorldState, second : ExtendedWorldState) {
+            return 1;
+        }
+    }Þ
 }
