16afc74ecad0f7ba6159349efb87b88c995b9f22
Markus Lindelöw <markus@lindeloew.se>
2015-03-28 10:24:39 +0100

    Added primtive types to Graph class

--- A_STAR.ts
+++ A_STAR.ts
@@ -1,80 +1,79 @@
-class Graph
-{
-    nodes : list;
-    edges : list;
+class Graph {
+    noOfNodes: number;
+    edges: [number, number][][]; //A list of tuples for every node
     
-    constructor(nodes,edges)
+    constructor(noOfNodes : number, edges : [number, number][][])
     {
-	this.nodes=nodes;
-	this.edges=edges;
+        this.noOfNodes = noOfNodes;
+	    this.edges = edges;
     }
 
-    function findPath(start,goal)
+    findPath(start : number, goal : number) : [number, number][] //Returns a list of edges
     {
-	var closedset = []; //list
-	var openset = start; //list
-	var came_from = []; //matris typ
+        if (start > this.noOfNodes || start <= 0 || goal > this.noOfNodes || goal < 0)
+            throw new RangeError("node does not exist");
 
-	var g_score = ; //array of same length as nodes list
-	g_score[start]=0;
+	    var closedset = []; //list
+	    var openset = start; //list
+        var came_from = []; //matris typ
 
-	//min heap på f_score
-	f_score[start]=g_score[start] + heuristicCost(start,goal);
+        /*
+        var f_score: minheap = new MinHeap();
 
-	While(openset.size() != 0)
-	{
-	    current=//hitta elementet med lägst värde i f_score, sätt current till dess index
+	    var g_score = ; //array of same length as nodes list
+	    g_score[start]=0;
 
-	    if(current==goal)
-	    {
-		//hurray
-		//returnera
-	    }
+	    //min heap på f_score
+	    f_score[start] = g_score[start] + heuristicCost(start,goal);
 
-	    //find current in openset and remove that element and add to closed
-	    it=openset.find(current);
-	    closedset.add(openset(it));
-	    openset.remove(it);
+        while (openset.size() != 0) {
+            var current: number = ;//hitta elementet med lägst värde i f_score, sätt current till dess index
 
-	    current_neighbour=getNeighbours(current);
+            if (current == goal) {
+                //hurray
+                //returnera
+            }
 
-	    for(var i = 0; i< current_neighbour.length ; ++i)
-	    {
-		if(closedSet.contains(current_neighbour[i]))
-		    continue;
+            //find current in openset and remove that element and add to closed
+            it = openset.find(current);
+            closedset.add(openset(it));
+            openset.remove(it);
 
-		var tentative_g_score=g_score[current] + edge_between_cost;
+            current_neighbour = getNeighbours(current);
 
-		if( !openset.contains(current_neighbour[i]) || tenative_g_score < g_score[current_neighbour[i]] )
-		{
-		    //spara undan hur du kom hit
-		    g_score[current_neighbour[i]]=tenative_g_score;
-		    f_score[current_neighbour[i]]=g_score[current_neighbour[i]] + heuristicCost[current_neighbour[i],goal];
-		    if(openset.contains(current_neighbour[i]))
-			openset.add(current_neighbour[i]);
-		}
-	    }
+            for (var i = 0; i < current_neighbour.length; ++i) {
+                if (closedSet.contains(current_neighbour[i]))
+                    continue;
 
-	    return null;
-	    
+                var tentative_g_score = g_score[current] + edge_between_cost;
+
+                if (!openset.contains(current_neighbour[i]) || tenative_g_score < g_score[current_neighbour[i]]) {
+                    //spara undan hur du kom hit
+                    g_score[current_neighbour[i]] = tenative_g_score;
+                    f_score[current_neighbour[i]] = g_score[current_neighbour[i]] + heuristicCost[current_neighbour[i], goal];
+                    if (openset.contains(current_neighbour[i]))
+                        openset.add(current_neighbour[i]);
+                }
+            }
+        }
+        */
+        return null; //No path was found
 	}
-	
-    }
 
-    function heuristicCost(current,goal)
+    heuristicCost(current : number, goal : number) : number
     {
+        return 0;
     }
 
-    function getNeighbours(node)
+    getNeighbours(node : number) : [number, number][]
     {
+        if (node < 0 || node >= this.noOfNodes)
+            throw new RangeError("Node does not exist");
+
+        return this.edges[node];
     }
-    
-}
 
-
-
-function Astar(start, goal,nodes,edges)
-{
-    var evaluated_node=[];
-    var open_set=start;
+    get NoOfNodes() : number {
+        return this.noOfNodes;
+    }
 }
