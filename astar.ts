d481d2fdc9659d5f18b94def26874d8f17814d9e
Markus Lindelöw <markus@lindeloew.se>
2015-03-28 19:38:26 +0100

    Added a view for a-star

---
+++ astar.ts
@@ -0,0 +1,23 @@
+﻿///<reference path="lib/jquery.d.ts"/>
+///<reference path="europe.ts"/>
+///<reference path="Graph.ts"/>
+
+for (var n in Europe.NodeNames)
+{
+    $('#from').append('<option>' + Europe.NodeNames[n] + '</option>');
+    $('#to').append('<option>' + Europe.NodeNames[n] + '</option>');
+}
+
+var G = new Graph(Europe.NoOfNodes, Europe.Edges);
+
+$('#route').click(function () {
+    var from = $('#from').find(":selected").index();
+    var to = $('#to').find(":selected").index();
+    var path = G.findPath(from, to);
+
+
+    $('#result').text('');
+    $('#result').append(Europe.NodeNames[path[0][0]] + ' ');
+    for (var i in path)
+        $('#result').append(Europe.NodeNames[path[i][1]] + ' ');
+});
