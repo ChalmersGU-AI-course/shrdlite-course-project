2a8aec4c22a916320cf17551bc90dfce046cabc6
Markus Lindelöw <markus@lindeloew.se>
2015-03-29 13:19:11 +0200

    Added a map

--- astar.ts
+++ astar.ts
@@ -2,30 +2,55 @@
 ///<reference path="europe.ts"/>
 ///<reference path="Graph.ts"/>
 
-for (var n in Europe.NodeNames)
-{
-    $('#from').append('<option>' + Europe.NodeNames[n] + '</option>');
-    $('#to').append('<option>' + Europe.NodeNames[n] + '</option>');
-}
+function init(): void {
+    
+    //plot map
 
-var G = new Graph(Europe.NoOfNodes, Europe.Edges);
+    var canv = document.createElement("canvas");
+    canv.width = 600;
+    canv.height = 513;
+    $('#map').append(canv);
+    var ctx = canv.getContext('2d');
 
-$('#route').click(function () {
-    var from = $('#from').find(":selected").index();
-    var to = $('#to').find(":selected").index();
-    var path = G.findPath(from, to);
+    var bild = new Image();
+    bild.src = "europe.svg";
+    bild.onload = function () { ctx.drawImage(bild, 0, 0, 600, 513); };
 
-    if (path != null)
-    {
-        $('#result').text('');
-        $('#result').append(Europe.NodeNames[path[0][0]] + ' ');
-        for (var i in path)
-            $('#result').append(Europe.NodeNames[path[i][1]] + ' ');
-    }
-    else
-    {
-        $('#result').text('No route possible');
+    for (var n in Europe.Nodes) {
+        $('#from').append('<option>' + Europe.Nodes[n].name + '</option>');
+        $('#to').append('<option>' + Europe.Nodes[n].name + '</option>');
     }
 
+    var G = new Graph(Europe.Nodes, Europe.Edges);
 
-});
+    $('#route').click(function () {
+        var from = $('#from').find(":selected").index();
+        var to = $('#to').find(":selected").index();
+        var path = G.findPath(from, to);
+
+        if (path != undefined) {
+            $('#result').text('');
+            var startNode: GraphNode = Europe.Nodes[path[0][0]];
+            $('#result').append(startNode.name + ' ');
+
+            ctx.clearRect(0, 0, 600, 513);
+            ctx.drawImage(bild, 0, 0, 600, 513);
+
+            ctx.beginPath();
+            ctx.moveTo(startNode.x, startNode.y);
+
+            for (var i in path)
+            {
+                $('#result').append(Europe.Nodes[path[i][1]].name + ' ');
+                ctx.lineTo(Europe.Nodes[path[i][1]].x, Europe.Nodes[path[i][1]].y);
+            }
+            ctx.strokeStyle = "black";
+            ctx.stroke();
+        }
+        else {
+            $('#result').text('No route possible');
+        }
+    });
+};
+
+$(document).ready(init);
