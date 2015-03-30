b68c3a40e578ae17d81561f864219205d72bf992
Lukas Borin <kurtyan@student.chalmers.se>
2015-03-30 15:41:39 +0200

    A start skeleton

--- astar.ts
+++ astar.ts
@@ -1,2 +1,19 @@
 ///<reference path="typescript-collections/Collections.ts"/>
 
+module Astar
+{
+	export class Path<T>
+	{
+		nodes: T[];
+		constructor(i : number) { }
+	}
+
+	export function findPath<T>(start : T, end : T, 
+								gen : (t : T) => T[],
+								heuristic : (t : T) => number)  : Path<T>
+	{
+
+		
+		return new Path<T>(0);
+	}
+}
