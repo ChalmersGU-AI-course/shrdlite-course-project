6f9cafc4929f7616d87e4308f7216366f369b1de
Hjalmar Karlsson <hka@xao.se>
2015-03-30 22:40:41 +0200

    Implementing A* with primitive classes, not done yet but its a bit on the way, thougt I should commit so there isnt someonelse doing the sam.

--- Graph.ts
+++ Graph.ts
@@ -19,43 +19,52 @@ class Graph {
             throw new RangeError("Node does not exist");
 
         var closedset = []; //list
-        var openset = start; //list
+        var openset = []
+	openset[0]=start; //list
         var came_from = []; //matris typ
 
-        /*
-        var f_score: minheap = new MinHeap();
+        //can't get stl to work
+        //var f_score: minheap = new MinHeap();
+	var f_score = []
 
-	    var g_score = ; //array of same length as nodes list
-	    g_score[start]=0;
+	//fill array with -1 to indicate nodes not yet calculated
+	for(var i=0; i < this.nodes.length ; ++i)
+	    f_score[i]=-1;
 
-	    //min heap på f_score
-	    f_score[start] = g_score[start] + heuristicCost(start,goal);
+	var g_score = []; //could maybe use = new int[f_score.length] for pre allocation
+	g_score[start]=0;
 
-        while (openset.size() != 0) {
-            var current: number = ;//hitta elementet med lägst värde i f_score, sätt current till dess index
+	//min heap på f_score
+	f_score[start] = g_score[start] + this.heuristicCost(start,goal);
 
+	//loop commented out to not get stuck in inf loop
+        //while (openset.length != 0) {
+            var current: number = this.indexOfSmallest(f_score);//hitta elementet med lägst värde i f_score, sätt current till dess index
+	    
             if (current == goal) {
                 //hurray
-                //returnera
+		return came_from;
             }
 
             //find current in openset and remove that element and add to closed
-            it = openset.find(current);
-            closedset.add(openset(it));
-            openset.remove(it);
+            var it:number = this.find(openset,current);
+            closedset.push(openset[it]);
+	    openset.splice(it,1); //splice removes the element at index
 
-            current_neighbour = getNeighbours(current);
-
-            for (var i = 0; i < current_neighbour.length; ++i) {
-                if (closedSet.contains(current_neighbour[i]))
+	   
+	    current_neighbours = this.getNeighbours(current);
+	
+	    /*
+            for (var i = 0; i < current_neighbours.length; ++i) {
+                if (closedSet.contains(current_neighbours[i]))
                     continue;
 
                 var tentative_g_score = g_score[current] + edge_between_cost;
 
-                if (!openset.contains(current_neighbour[i]) || tenative_g_score < g_score[current_neighbour[i]]) {
+                if (!openset.contains(current_neighbours[i]) || tenative_g_score < g_score[current_neighbours[i]]) {
                     //spara undan hur du kom hit
-                    g_score[current_neighbour[i]] = tenative_g_score;
-                    f_score[current_neighbour[i]] = g_score[current_neighbour[i]] + heuristicCost[current_neighbour[i], goal];
+                    g_score[current_neighbours[i]] = tenative_g_score;
+                    f_score[current_neighbours[i]] = g_score[current_neighbours[i]] + heuristicCost[current_neighbour[i], goal];
                     if (openset.contains(current_neighbour[i]))
                         openset.add(current_neighbour[i]);
                 }
@@ -78,6 +87,22 @@ class Graph {
             throw new RangeError("Node does not exist");
 
         return this.edges[node];
+    }
+
+    //returns index of smallest non negative element
+    indexOfSmallest(arr:number[]) {
+	var lowest = 0;
+	for (var i = 1; i < arr.length; ++i) {
+	    if ( (arr[i] < arr[lowest] && arr[i]>=0 ) || (arr[i]>=0 && arr[lowest]==-1) ) lowest = i;
+	}
+	return lowest;
+    }
+    find(arr:number[],value:number) {
+	var index = -1;
+	for (var i = 0; i < arr.length; ++i) {
+	    if ( arr[i] == value ) index = i;
+	}
+	return index;
     }
 
     get NoOfNodes(): number {
