d3ef7319c9dc55f36d960e8c638cf9d3630efe32
axhav <william_axhav@hotmail.com>
2015-03-30 13:58:05 +0200

    added the bucharest example

--- AStar.js
+++ AStar.js
@@ -2328,18 +2328,138 @@ var AStar;
         }
         return 0;
     }
-    function asdasdJarnaMain(a, b) {
-        //var queue = new collections.PriorityQueue<Test>(compareT);
+    function example1(a, b) {
         var graph = {};
-        graph[0] = { id: 0, hweight: 0, neighbours: [[2, 1], [4, 2], [1, 3]] };
-        graph[1] = { id: 1, hweight: 15, neighbours: [[2, 0], [1, 2], [10, 3]] };
-        graph[2] = { id: 2, hweight: 30, neighbours: [[2, 0], [2, 1], [2, 3]] };
-        graph[3] = { id: 3, hweight: 5, neighbours: [[2, 1], [2, 2]] };
-        var ret = astar(a, b, graph);
+        graph[0] = { id: 1, hweight: 10, neighbours: [[2, 1], [4, 2], [1, 3]] };
+        graph[1] = { id: 2, hweight: 15, neighbours: [[2, 0], [1, 2], [2, 3]] };
+        graph[2] = { id: 3, hweight: 30, neighbours: [[2, 0], [2, 1], [2, 3]] };
+        graph[3] = { id: 4, hweight: 5, neighbours: [[2, 1], [2, 2]] };
+        var ret = astar(a, b, example1HeurFunction, graph);
+        console.log(ret);
         return ret;
     }
-    AStar.asdasdJarnaMain = asdasdJarnaMain;
-    function astar(start, goal, graph) {
+    AStar.example1 = example1;
+    function example1HeurFunction(t) {
+        return t.hweight;
+    }
+    function example2() {
+        var graph2 = {};
+        graph2[0] = { id: 0, neighbours: [[75, 19], [118, 16], [140, 15]] };
+        graph2[1] = { id: 1, neighbours: [[211, 5], [85, 18], [90, 6], [101, 13]] };
+        graph2[2] = { id: 2, neighbours: [[120, 3], [146, 14], [138, 13]] };
+        graph2[3] = { id: 3, neighbours: [[75, 10], [120, 2]] };
+        graph2[4] = { id: 4, neighbours: [[86, 7]] };
+        graph2[5] = { id: 5, neighbours: [[99, 15], [80, 14], [140, 0], [151, 12]] };
+        graph2[6] = { id: 6, neighbours: [[90, 1]] };
+        graph2[7] = { id: 7, neighbours: [[98, 17], [86, 4]] };
+        graph2[8] = { id: 8, neighbours: [[87, 11], [92, 18]] };
+        graph2[9] = { id: 9, neighbours: [[111, 16], [70, 10]] };
+        graph2[10] = { id: 10, neighbours: [[70, 9], [75, 3]] };
+        graph2[11] = { id: 11, neighbours: [[87, 8]] };
+        graph2[12] = { id: 12, neighbours: [[71, 19], [151, 15]] };
+        graph2[13] = { id: 13, neighbours: [[97, 14], [101, 1], [138, 2]] };
+        graph2[14] = { id: 14, neighbours: [[80, 15], [97, 13], [146, 2]] };
+        graph2[15] = { id: 15, neighbours: [[140, 0], [151, 12], [99, 5], [80, 14]] };
+        graph2[16] = { id: 16, neighbours: [[118, 0], [111, 9]] };
+        graph2[17] = { id: 17, neighbours: [[85, 1], [142, 18], [98, 7]] };
+        graph2[18] = { id: 18, neighbours: [[92, 8], [142, 17]] };
+        graph2[19] = { id: 19, neighbours: [[71, 12], [75, 0]] };
+        var optPath = astar(0 /* Arad */, 1 /* Bucharest */, example2HeurFunction, graph2);
+        for (var i in optPath) {
+            var j = i;
+            console.log(i - (-1), City[optPath[(optPath.length - 1) - i]]);
+        }
+    }
+    AStar.example2 = example2;
+    var City;
+    (function (City) {
+        City[City["Arad"] = 0] = "Arad";
+        City[City["Bucharest"] = 1] = "Bucharest";
+        City[City["Chraiova"] = 2] = "Chraiova";
+        City[City["Dobreta"] = 3] = "Dobreta";
+        City[City["Eforie"] = 4] = "Eforie";
+        City[City["Fagaras"] = 5] = "Fagaras";
+        City[City["Giurgiu"] = 6] = "Giurgiu";
+        City[City["Hirsova"] = 7] = "Hirsova";
+        City[City["Iasi"] = 8] = "Iasi";
+        City[City["Lugoj"] = 9] = "Lugoj";
+        City[City["Mehadia"] = 10] = "Mehadia";
+        City[City["Neamt"] = 11] = "Neamt";
+        City[City["Oreada"] = 12] = "Oreada";
+        City[City["Pitesti"] = 13] = "Pitesti";
+        City[City["RimnicuVilecea"] = 14] = "RimnicuVilecea";
+        City[City["Sibiu"] = 15] = "Sibiu";
+        City[City["Timisoara"] = 16] = "Timisoara";
+        City[City["Urziceni"] = 17] = "Urziceni";
+        City[City["Vaslui"] = 18] = "Vaslui";
+        City[City["Zerind"] = 19] = "Zerind";
+    })(City || (City = {}));
+    ;
+    function example2HeurFunction(input) {
+        switch (input) {
+            case 0:
+                return 366;
+                break;
+            case 1:
+                return 0;
+                break;
+            case 2:
+                return 160;
+                break;
+            case 3:
+                return 242;
+                break;
+            case 4:
+                return 161;
+                break;
+            case 5:
+                return 178;
+                break;
+            case 6:
+                return 77;
+                break;
+            case 7:
+                return 151;
+                break;
+            case 8:
+                return 226;
+                break;
+            case 9:
+                return 244;
+                break;
+            case 10:
+                return 241;
+                break;
+            case 11:
+                return 234;
+                break;
+            case 12:
+                return 380;
+                break;
+            case 13:
+                return 98;
+                break;
+            case 14:
+                return 193;
+                break;
+            case 15:
+                return 253;
+                break;
+            case 16:
+                return 329;
+                break;
+            case 17:
+                return 80;
+                break;
+            case 18:
+                return 199;
+                break;
+            case 19:
+                return 374;
+                break;
+        }
+    }
+    function astar(start, goal, huerFunction, graph) {
         var closedSet = [];
         var openSet = new collections.PriorityQueue(compareT);
         openSet.add(graph[start]);
@@ -2350,27 +2470,19 @@ var AStar;
         f_score[start] = g_score[start] + graph[start].hweight;
         var indextest = 0;
         while (!openSet.isEmpty()) {
-            console.log(indextest++);
             var current = openSet.dequeue();
             if (current.id == goal) {
                 return reconstruct_path(came_from, goal);
             }
             closedSet.push(current.id);
-            console.log(current.id);
             for (var ei in current.neighbours) {
                 var e = current.neighbours[ei];
-                //console.log("asdasdasd ",  e);
-                //console.log(!arrayIsMember(e[1],closedSet));
                 if (!arrayIsMember(e[1], closedSet)) {
                     var tentative_g_score = g_score[current.id] + e[0];
-                    if (!openSet.contains(graph[e[1]])) {
-                        g_score[e[1]] = 2147483647;
-                    }
-                    if (tentative_g_score < g_score[e[1]]) {
-                        //console.log("asdasd");
+                    if (!openSet.contains(graph[e[1]]) || tentative_g_score < g_score[e[1]]) {
                         came_from[e[1]] = current.id;
                         g_score[e[1]] = tentative_g_score;
-                        f_score[e[1]] = g_score[e[1]] + graph[e[1]].hweight;
+                        f_score[e[1]] = g_score[e[1]] + huerFunction(graph[e[1]]);
                         graph[e[1]].fscore = f_score[e[1]];
                         openSet.enqueue(graph[e[1]]);
                     }
--- AStar.ts
+++ AStar.ts
@@ -5,7 +5,7 @@ module AStar {
     interface Test
     {
         id: number;
-        hweight: number;
+        hweight?: number;
         neighbours: [number,number][];
         fscore?: number;
     }
@@ -29,21 +29,148 @@ module AStar {
         return 0
     }
     
-    export function asdasdJarnaMain(a : number, b : number)
+    export function example1(a : number, b : number)
     {
-        
-        //var queue = new collections.PriorityQueue<Test>(compareT);
-        
         var graph: { [key:number] : Test; } = {};
-        graph[0] = {id:0,hweight:0,neighbours:[[2,1] ,[4,2],[1,3]]};
-        graph[1] = {id:1,hweight:15,neighbours:[[2,0],[1,2],[10,3]]};
-        graph[2] = {id:2,hweight:30,neighbours:[[2,0],[2,1],[2,3]]};  
-        graph[3] = {id:3,hweight:5,neighbours:[[2,1],[2,2]]};
-        var ret = astar(a,b,graph);
+        graph[0] = {id:1,hweight:10,neighbours:[[2,1] ,[4,2],[1,3]]};
+        graph[1] = {id:2,hweight:15,neighbours:[[2,0],[1,2],[2,3]]};
+        graph[2] = {id:3,hweight:30,neighbours:[[2,0],[2,1],[2,3]]};  
+        graph[3] = {id:4,hweight:5,neighbours:[[2,1],[2,2]]};
+        var ret = astar(a,b,example1HeurFunction,graph);
+        console.log(ret)
         return ret;
     }
     
-    function astar(start,goal,graph:{ [key:number] : Test; }) : number[]
+    function example1HeurFunction(t : Test) : number
+    {
+        return t.hweight;
+    }
+    
+    export function example2(input:number)
+    {
+        var graph2: { [key:number] : Test; } = {};
+        graph2[0] = {id:0,neighbours:[[75,19],[118,16],[140,15]]};
+        graph2[1] = {id:1,neighbours:[[211,5],[85,18],[90,6],[101,13]]};
+        graph2[2] = {id:2,neighbours:[[120,3],[146,14],[138,13]]};
+        graph2[3] = {id:3,neighbours:[[75,10],[120,2]]};
+        graph2[4] = {id:4,neighbours:[[86,7]]};
+        graph2[5] = {id:5,neighbours:[[99,15],[80,14],[140,0],[151,12]]};
+        graph2[6] = {id:6,neighbours:[[90,1]]};
+        graph2[7] = {id:7,neighbours:[[98,17],[86,4]]};
+        graph2[8] = {id:8,neighbours:[[87,11],[92,18]]};
+        graph2[9] = {id:9,neighbours:[[111,16],[70,10]]};
+        graph2[10] = {id:10,neighbours:[[70,9],[75,3]]};
+        graph2[11] = {id:11,neighbours:[[87,8]]};
+        graph2[12] = {id:12,neighbours:[[71,19],[151,15]]};
+        graph2[13] = {id:13,neighbours:[[97,14],[101,1],[138,2]]};
+        graph2[14] = {id:14,neighbours:[[80,15],[97,13],[146,2]]};
+        graph2[15] = {id:15,neighbours:[[140,0],[151,12],[99,5],[80,14]]};
+        graph2[16] = {id:16,neighbours:[[118,0],[111,9]]};
+        graph2[17] = {id:17,neighbours:[[85,1],[142,18],[98,7]]};
+        graph2[18] = {id:18,neighbours:[[92,8],[142,17]]};
+        graph2[19] = {id:19,neighbours:[[71,12],[75,0]]};
+        
+        var optPath : number[] = astar(input,City.Bucharest,example2HeurFunction,graph2);
+        for(var i in optPath)
+        {
+            console.log(i-(-1),City[optPath[(optPath.length-1)-i]]);
+        }
+        
+    }
+    enum City 
+    {
+        Arad,
+        Bucharest,
+        Chraiova,
+        Dobreta,
+        Eforie,
+        Fagaras,
+        Giurgiu,
+        Hirsova,
+        Iasi,
+        Lugoj,
+        Mehadia,
+        Neamt,
+        Oreada,
+        Pitesti,
+        RimnicuVilecea,
+        Sibiu,
+        Timisoara,
+        Urziceni,
+        Vaslui,
+        Zerind
+    };
+        
+        
+    function example2HeurFunction(input:number):number
+    {
+        switch (input)
+        {
+        case 0: 
+            return  366;
+            break;
+        case 1: 
+            return  0;
+            break;
+        case 2: 
+            return  160;
+            break;
+        case 3: 
+            return  242;
+            break;
+        case 4: 
+            return  161;
+            break;
+        case 5: 
+            return  178;
+            break;
+        case 6: 
+            return  77;
+            break;
+        case 7: 
+            return  151;
+            break;    
+        case 8: 
+            return  226;
+            break;
+        case 9: 
+            return  244;
+            break;
+        case 10: 
+            return  241;
+            break;
+        case 11: 
+            return  234;
+            break;
+        case 12: 
+            return  380;
+            break;
+        case 13: 
+            return  98;
+            break;
+        case 14: 
+            return  193;
+            break;
+        case 15: 
+            return  253;
+            break;
+        case 16: 
+            return  329;
+            break;
+        case 17: 
+            return  80;
+            break;
+        case 18: 
+            return  199;
+            break; 
+        case 19: 
+            return  374;
+            break;
+        }
+    }
+    
+    
+    function astar(start,goal,huerFunction,graph:{ [key:number] : Test; }) : number[]
     {
         var closedSet : number[] = [];
         var openSet = new collections.PriorityQueue<Test>(compareT);
@@ -60,8 +187,7 @@ module AStar {
         
         var indextest : number = 0;
         while(!openSet.isEmpty())
-        {   
-            console.log(indextest++);
+        {
             var current = openSet.dequeue();
             if (current.id == goal)
             {
@@ -69,25 +195,19 @@ module AStar {
             }
             
             closedSet.push(current.id); 
-            console.log(current.id);
             for(var ei in current.neighbours)
             {
                 var e : number[] = current.neighbours[ei];
-                //console.log("asdasdasd ",  e);
-                //console.log(!arrayIsMember(e[1],closedSet));
+                
                 if (!arrayIsMember(e[1],closedSet))
                 {
                     var tentative_g_score :number = g_score[current.id] + e[0];
-                    if(!openSet.contains(graph[e[1]]))
+                    
+                    if(!openSet.contains(graph[e[1]]) || tentative_g_score < g_score[e[1]])
                     {
-                        g_score[e[1]] = 2147483647;
-                    }
-                    if(tentative_g_score < g_score[e[1]])
-                    {
-                        //console.log("asdasd");
                         came_from[e[1]] = current.id;
                         g_score[e[1]] = tentative_g_score;
-                        f_score[e[1]] = g_score[e[1]] + graph[e[1]].hweight;
+                        f_score[e[1]] = g_score[e[1]] + huerFunction(graph[e[1]]);
                         graph[e[1]].fscore = f_score[e[1]];
                         openSet.enqueue(graph[e[1]]);
                     }
@@ -109,7 +229,7 @@ module AStar {
         return total_path;
     }
 
-    function arrayIsMember (e , array)
+    function arrayIsMember (e , array) : boolean
     {
         for(var i in array)
         {
