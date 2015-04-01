bec5d8ec27891376bdc058a74fc7d3685f1d05a5
johannesringmark <johannes.ringmark@gmail.com>
2015-04-01 17:23:49 +0200

    Delete, Dictionary used for backtracking

--- astarAlgorithm.ts
+++ astarAlgorithm.ts
@@ -3,7 +3,7 @@ module Astar {
     //n.state:,
     //n.parent:,
     //n.action:,
- export class Node{
+  export class Node{
     id: string;
     fscore: number;
     gscore: number; 
@@ -23,7 +23,7 @@ TODO: config priority que to sort by f(x)
 
 AStar :: Graph -> Path
 */
- export function Astar(start: Node, goal:Node, functions : Functions): Node[]{
+  export function Astar(start: Node, goal:Node, functions : Functions): Node[]{
     //Node comparion function
     function comp(a:Node,b:Node){
       if (a.fscore < b.fscore)
@@ -36,12 +36,11 @@ AStar :: Graph -> Path
     }
     // Initilization Vendor Types 
     var closedset = new collections.PriorityQueue<Node>(); // nodes allready evaluated.
-    var queue =  new collections.PriorityQueue<Node>(comp); // workes in paralell with opensset
+    var openset =  new collections.PriorityQueue<Node>(comp); // workes in paralell with opensset
     // Initial calculations 
     start.gscore = 0; // the inital distace 
     start.fscore = start.gscore + functions.heuristic_approx(start,goal);
     openset.add(start);
-    queue.add(start); 
     // Variable initiations moved outside reduce redundancy 
     var current : Node
     var neighbors :[Node]
@@ -58,47 +57,43 @@ AStar :: Graph -> Path
         It checks if the current node is the goal node,
         if it is then finds and returns the path.
         */
-        current = queue.dequeue(); 
+        current = openset.dequeue(); 
         if(current === goal){
           var path = new collections.LinkedList<Node>();
-            path.add(current)
-              while(came_from.getValue(current)){
-                current = came_from.getValue(current)
-                path.add(current);
-              }
-              path.reverse
-              return path.toArray()
+          path.add(current)
+          while(current.successor){
+              current = (current).successor;
+              path.add(current);
+          }
+          path.reverse
+          return path.toArray()
         }
         /*
         All modifying actions performed on the set will also have to be performed
         on the queue.
         TODO : make the queue sort its element by f(x).   
         */
-        openset.remove(current)
-        queue.dequeue() 
         closedset.add(current)
         neighbors = current.children // expand the node that is first in the queue.
          /*
         All the neigbors are checked, in several ways    
         */ 
-        for(var neighbor in neighbors){
-            if (!closedset.contains(neighbor)){
-                g_score = current.gscore + functions.dist_between(current,neighbor) 
-            }else{ // else if - is to prefer. 
-            if (!openset.contains(neighbor) || neighbor.gscore < g_score){ // checks if the new path is better 
-              came_from.setValue(neighbor,current)
-              neighbor.gscore = g_score
-              neighbor.fscore = neighbor.gscore + functions.heuristic_approx(start,goal);
-                if (!openset.contains(neighbor)){
-                  // The neighbor has passed all checks and the node is added, before the next node is considered.  
-                  openset.add(neighbor)
-                  queue.add(neighbor); 
+        for(var n in neighbors){
+            if (!closedset.contains(neighbors[n])){
+                var g_score = current.gscore + functions.dist_between(current,neighbors[n])
+                if (!openset.contains(neighbors[n]) || neighbors[n].gscore < g_score){ // checks if the new path is better 
+                    neighbors[n].successor = current
+                    neighbors[n].gscore = g_score
+                    neighbors[n].fscore = neighbors[n].gscore + functions.heuristic_approx(neighbors[n],goal);
+                    if (!openset.contains(neighbors[n])){
+                        // The neighbors[n] has passed all checks and the node is added, before the next node is considered.  
+                        openset.add(neighbors[n]); 
+                    }
                 }
             }
-          }
-      }
+        }
+    }
+    //     if we end up here then there is no path between start and goal  
+    throw new Error("no path!");
   }
-   //     if we end up here then there is no path between start and goal  
-  throw new Error("no path!");
- }
 };
