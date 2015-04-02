7c7767872f4f8fe8cc8af069d281fc7710995222
Carsten Griesheimer <hallo@carstengriesheimer.de>
2015-04-03 00:48:58 +0200

    Test for different paths

--- test/astar_example.ts
+++ test/astar_example.ts
@@ -3,18 +3,39 @@
 
 var AStar = require("../src/AStar.js");
 
-var node1 = new AStar.Node("state1");
-var node2 = new AStar.Node("state2");
-var node3 = new AStar.Node("state3");
-
-node1.addNeighbour(node2, 1);
-node1.addNeighbour(node3, 2);
-node2.addNeighbour(node3, 3);
-node3.addNeighbour(node2, 4);
-
-function heuristic(start, goal): number {
-  return Math.random();
+function contents(nodes) {
+  return nodes.map((n) => n.content);
 }
 
-var result = AStar.astar(node1, node3, heuristic);
-console.log("result: ", result);
+function heuristic(start, goal): number {
+  return 1;
+}
+
+function example1() {
+  var node1 = new AStar.Node("state1");
+  var node2 = new AStar.Node("state2");
+  var node3 = new AStar.Node("state3");
+
+  node1.addNeighbour(node2, 1);
+  node1.addNeighbour(node3, 1);
+  node2.addNeighbour(node3, 1);
+
+  var result = AStar.astar(node1, node3, heuristic);
+  console.log("example 1: ", contents(result));
+}
+
+function example2() {
+  var node1 = new AStar.Node("state1");
+  var node2 = new AStar.Node("state2");
+  var node3 = new AStar.Node("state3");
+
+  node1.addNeighbour(node2, 1);
+  node1.addNeighbour(node3, 200);
+  node2.addNeighbour(node3, 1);
+
+  var result = AStar.astar(node1, node3, heuristic);
+  console.log("example 2: ", contents(result));
+}
+
+example1();
+example2();
