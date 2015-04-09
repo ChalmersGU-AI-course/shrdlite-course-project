1c10b93ec75f33df2b45b027f93b5f34eb6b5c1e
starpie <grankvist.david@gmail.com>
2015-04-09 17:09:15 +0200

    add test case with non-perfect heuristics

--- astar/TestCases.ts
+++ astar/TestCases.ts
@@ -53,6 +53,54 @@ module AStarTestCases {
 
     }
 
+    // Creates an example graph and runs AStar on it
+    export function testCase2() {
+
+        // Define graph
+        var a = new AStar.Node("a");
+        var b = new AStar.Node("b");
+        var c = new AStar.Node("c");
+        var d = new AStar.Node("d");
+        var e = new AStar.Node("e");
+        var f = new AStar.Node("f");
+        var g = new AStar.Node("g");
+        var h = new AStar.Node("h");
+        var h = new AStar.Node("i");
+        var nodes = [a,b,c,d,e,f,g,h,i];
+        var edges = AStar.Edge.createEdges([[a,b,5], [b,c,1], [c,d,1], [d,e,1], 
+            [e,f,1], [f,g,1], [g,h,1], [g,c,2], [h,i,1], [a,i,1]);
+
+        initGraph(nodes, edges); // Updates node objects to be a proper graph
+
+        //A simple heuristic function that returns the number of edges from the goal to the given node
+        var heuristic = function(node) {
+            if(node.label === "a") return 2
+            if(node.label === "b") return 1
+            if(node.label === "c") return 0
+            if(node.label === "d") return 1
+            if(node.label === "e") return 2
+            if(node.label === "f") return 2
+            if(node.label === "g") return 1
+            if(node.label === "h") return 2
+            if(node.label === "i") return 3
+        }
+
+        console.log("Running astar correctness test ... ");
+        var path = AStar.astar(a, d, nodes, heuristic);
+        var correctPath = [a,i,h,g,c];
+        if (!test(arrayEquals(path, correctPath)))
+            console.log("nodes: ",nodes);
+
+        console.log("Running astar heuristics test ... ");
+        if (!test(d.previous===null && e.previous===null && f.previous===null)) {
+            console.log("nodes: ",nodes);
+            console.log("d.previous:", d.previous);
+            console.log("e.previous:", e.previous);
+            console.log("f.previous:", f.previous);
+        }
+
+    }
+
     function test(test: boolean) : boolean {
         console.log(test? "... passed!" : "... FAILED!");
         return test;
