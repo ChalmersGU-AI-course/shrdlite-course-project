8f13f0e96bfd318b71dc5a02a9a5811f99813340
sfabian <me@sfabian.net>
2015-04-20 16:48:54 +0200

    Define the PDDL language

---
+++ cgi-bin/PDDL.py
@@ -0,0 +1,47 @@
+from enum import Enum
+
+class Size(Enum):
+  small = 1
+  large = 2
+
+class Color(Enum):
+  red = 1
+  black = 2
+  blue = 3
+  green = 4
+  yellow = 5
+  white = 6
+
+class Form(Enum):
+  brick = 1
+  plank = 2
+  ball = 3
+  pyramid = 4
+  box = 5
+  table = 6
+  floor = 7
+
+class Entity:
+  def __init__(self, size, color, form):
+    self.size = size
+    self.color = color
+    self.form = form
+
+class Spatial(Enum):
+  on = 1
+  above = 2
+  under = 3
+  beside = 4
+  left-of = 5
+  right-of = 6
+  strictly-right-of = 7
+
+class Relation:
+  def __init__(self, spatial, a, b):
+    self.spatial = spatial
+    self.a = a
+    self.b = b
+
+# POSSIBLE MORE OF THESE FOR ALL RELATIONS???
+def ON(a, b):
+  return Relation(Spatial.on, a, b)
