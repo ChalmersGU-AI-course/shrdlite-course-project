21c1e7291f3d1e325ed1cf55d6711648b1881f1a
Krabban <anderssongabriel665@gmail.com>
2015-04-22 16:24:05 +0200

    created dummy functions in interpreter. interpret- object, entity, location

--- Interpreter.ts
+++ Interpreter.ts
@@ -10,6 +10,7 @@ module Interpreter {
         var interpretations : Result[] = [];
         parses.forEach((parseresult) => {
             var intprt : Result = <Result>parseresult;
+            console.log("\nInput is :"+parseresult.input);
             intprt.intp = interpretCommand(intprt.prs, currentState);
             interpretations.push(intprt);
         });
@@ -47,16 +48,53 @@ module Interpreter {
     // private functions
 
     function interpretCommand(cmd : Parser.Command, state : WorldState) : Literal[][] {
+        //console.log(cmd);
+
+        if(cmd.ent != null){
+            interpretEntity(cmd.ent, state);
+        }
+        if(cmd.loc != null){
+            interpretLocation(cmd.loc, state);
+        }
         // This returns a dummy interpretation involving two random objects in the world
         var objs : string[] = Array.prototype.concat.apply([], state.stacks);
         var a = objs[getRandomInt(objs.length)];
         var b = objs[getRandomInt(objs.length)];
         var intprt : Literal[][] = [[
-            {pol: true, rel: "ontop", args: [a, "floor"]},
-            {pol: true, rel: "holding", args: [b]}
+            {pol: false, rel: "ontop", args: [a, "floor"]},
+            {pol: false, rel: "holding", args: [b]}
         ]];
         return intprt;
     }
+
+    function interpretEntity(ent : Parser.Entity, state : WorldState) : Literal[][] {
+        //console.log(ent);
+        if(ent.obj != null){
+            interpretObject(ent.obj, state);
+        }
+
+        return null;
+    }
+
+    function interpretObject(obj : Parser.Object, state : WorldState) : Literal[][] {
+        //console.log(obj);
+        if(obj.loc != null){
+            interpretLocation(obj.loc, state);
+        }
+        if(obj.obj != null){
+            interpretObject(obj.obj, state);
+        }
+
+        return null;
+    }
+    function interpretLocation(loc : Parser.Location, state : WorldState) : Literal[][] {
+        //console.log(loc);
+        if(loc.ent != null){
+            interpretEntity(loc.ent, state);
+        }
+        return null;
+    }
+
 
 
     function getRandomInt(max) {
