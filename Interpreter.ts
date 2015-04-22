4721c1a66e2f54b68211ed0c8ed4dc0a123a3194
Daniel <d.linden88@gmail.com>
2015-04-22 12:12:02 +0200

    add fintObjects function to interpreter

--- Interpreter.ts
+++ Interpreter.ts
@@ -47,6 +47,8 @@ module Interpreter {
     // private functions
 
     function interpretCommand(cmd : Parser.Command, state : WorldState) : Literal[][] {
+        var matching = findObjects(cmd.ent, state);
+
         // This returns a dummy interpretation involving two random objects in the world
         var objs : string[] = Array.prototype.concat.apply([], state.stacks);
         var a = objs[getRandomInt(objs.length)];
@@ -58,6 +60,123 @@ module Interpreter {
         return intprt;
     }
 
+    function findObjects(entity: Parser.Entity, world: WorldState): string[] {
+        var parserObject = entity.obj;
+        if (parserObject.obj) {
+            return findObjectsByLocation(parserObject, world);
+        } else {
+            return findObjectsByDescription(parserObject, world);
+        }
+    }
+
+    function findObjectsByDescription(object: Parser.Object, world: WorldState): string[] {
+        var result: string[] = [];
+
+        if (world.holding) {
+            var objectDefinition = world.objects[world.holding];
+            if (isMatchByDescription(object, objectDefinition)) {
+                result.push(world.holding);
+            }
+        }
+        
+        for (var stack = 0; stack < world.stacks.length; ++stack) {
+            for (var objectnr = 0; objectnr < world.stacks[stack].length; ++objectnr) {
+                var worldObject = world.stacks[stack][objectnr];
+                var objectDefinition = world.objects[worldObject];
+                if (isMatchByDescription(object, objectDefinition)) {
+                    result.push(worldObject);
+                }
+            }
+        }
+        return result;
+    }
+
+    function isMatchByDescription(object: Parser.Object, objectDefinition: ObjectDefinition): boolean {
+        if (object.form !== "anyform" && object.form !== objectDefinition.form) {
+            return false;
+        }
+
+        if (object.size && object.size !== objectDefinition.size) {
+            return false;
+        }
+
+        if (object.color && object.color !== objectDefinition.color) {
+            return false;
+        }
+        return true;
+    }
+
+    function findObjectsByLocation(object: Parser.Object, world: WorldState): string[] {
+        var result: string[] = [];
+        var matchingObjects = findObjectsByDescription(object.obj, world);
+
+        for (var objectnr = 0; objectnr < matchingObjects.length; ++objectnr) {
+            var matchingObject = matchingObjects[objectnr];
+            if (isMatchByLocation(matchingObject, object.loc, world)) {
+                result.push(matchingObject);
+            }
+        }
+        return result;
+    }
+
+    function isMatchByLocation(objectId: string, location: Parser.Location, world: WorldState): boolean {
+        var matchingEntities = findObjects(location.ent, world);
+        for (var matchingNr = 0; matchingNr < matchingEntities.length; ++matchingNr) {
+            if (isRelativeMatch(objectId, location.rel, matchingEntities[matchingNr], world)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function isRelativeMatch(firstObject: string, relation: string, secondObject: string, world: WorldState): boolean {
+        var firstPosition = getObjectPosition(firstObject, world);
+        var secondPosition = getObjectPosition(secondObject, world);
+
+        if (!firstPosition || !secondPosition) {
+            return false;
+        }
+
+        switch (relation) {
+            case "leftof":
+                return firstPosition.Stack < secondPosition.Stack;
+            case "rightof":
+                return firstPosition.Stack > secondPosition.Stack;
+            case "beside":
+                return Math.abs(firstPosition.Stack - secondPosition.Stack) === 1;
+            case "inside":
+            case "ontop":
+                return firstPosition.Stack === secondPosition.Stack && 
+                       firstPosition.ObjectNr === secondPosition.ObjectNr + 1;
+            case "under":
+                return firstPosition.Stack === secondPosition.Stack && 
+                       firstPosition.ObjectNr < secondPosition.ObjectNr;
+            case "above":
+                return firstPosition.Stack === secondPosition.Stack && 
+                       firstPosition.ObjectNr > secondPosition.ObjectNr;
+        }
+        throw "Invalid relation '" + relation + "'";
+    }
+    class ObjectPosition {
+        constructor(
+            public Stack: number,
+            public ObjectNr: number
+        ) {}
+    }
+
+    function getObjectPosition(objectId: string, world: WorldState): ObjectPosition {
+        if (world.holding === objectId) {
+            return null;
+        }
+        for (var stack = 0; stack < world.stacks.length; ++stack) {
+            for (var objectnr = 0; objectnr < world.stacks[stack].length; ++objectnr) {
+                if (objectId === world.stacks[stack][objectnr]) {
+                    return new ObjectPosition(stack, objectnr);
+                }
+            }
+        }
+        throw "Unable to find object with id " + objectId;
+    }
 
     function getRandomInt(max) {
         return Math.floor(Math.random() * max);
