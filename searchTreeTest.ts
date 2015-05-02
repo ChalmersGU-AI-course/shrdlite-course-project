2d4a870d5dedd698cafbb4803f456e3e455a2336
Paul Lange <palango@gmx.de>
2015-05-02 15:00:00 +0200

    implement getNeighbors

--- searchTreeTest.ts
+++ searchTreeTest.ts
@@ -16,10 +16,77 @@ if (process.argv.length != 3 || !ExampleWorlds[worldname]) {
     process.exit(1);
 }
 
-var world = new TextWorld(ExampleWorlds[worldname]);
+var origState = ExampleWorlds[worldname];
+console.log(origState.holding);
+var world = new TextWorld(origState);
+
+origState.arm = 2;
+origState.holding = "e";
 world.printWorld();
 
-var origState = world.currentState;
-var copyDepth = 5; // 3 should be enough, lets be sure
-var copyState = owl.deepCopy(origState, copyDepth);
-new TextWorld(ExampleWorlds[worldname]).printWorld();
+console.log("Neighbors")
+
+var states = getNeighbors(origState);
+for (var i = states.length - 1; i >= 0; i--) {
+    new TextWorld(states[i]).printWorld();
+}
+
+
+function getNeighbors(state: WorldState): WorldState[]
+{
+
+    console.log(state.holding);
+    var newStates = [];
+
+    var useArmState = useArm(state);
+    if (useArmState) {
+        newStates.push(useArmState);
+    }
+
+    var moveLeftState = moveArm(state, -1);
+    if (moveLeftState) {
+        newStates.push(moveLeftState);
+    }
+
+    var moveRightState = moveArm(state, 1);
+    if (moveRightState) {
+        newStates.push(moveRightState);
+    }
+    return newStates;
+}
+
+function useArm(state: WorldState): WorldState
+{
+    if (state.holding === null) {
+        console.log(state.holding);
+        console.log("picking object up");
+        var currentStack = state.stacks[state.arm];
+        if (currentStack.length > 0) {
+            var topItemIndex = currentStack.length - 1;
+            var newState = owl.deepCopy(state, 5);
+            newState.holding = currentStack[topItemIndex];
+            newState.stacks[state.arm].splice(topItemIndex, 1);
+            return newState;
+        }
+    } else { // holding something at the moment
+        console.log("putting object down");
+        var newState = owl.deepCopy(state, 5);
+        //TODO: check if legal move
+        newState.stacks[newState.arm].push(newState.holding);
+        newState.holding = null;
+        return newState;
+    }
+    return null;
+}
+
+function moveArm(state: WorldState, direction: number): WorldState
+{
+    var numberOfStacks = state.stacks.length;
+    var targetPos = state.arm + direction;
+    if (targetPos >= 0 && targetPos < numberOfStacks) {
+        var newState = owl.deepCopy(state, 5);
+        newState.arm = targetPos;
+        return newState;
+    }
+    return null;
+}
