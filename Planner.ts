7bb108bb95f6b0e61a87df13e013a6f8177fcca0
Paul Lange <palango@gmx.de>
2015-05-03 22:42:38 +0200

    Move planning code to Planner.ts

--- Planner.ts
+++ Planner.ts
@@ -1,7 +1,8 @@
 ///<reference path="World.ts"/>
 ///<reference path="Interpreter.ts"/>
 
-///<reference path="planner_astar.ts"/>
+///<reference path="astar.ts" />
+///<reference path="deepCopy.ts"/>
 
 module Planner {
 
@@ -88,13 +89,13 @@ module Planner {
 
         var plan: string[] = [];
 
-        var graphGoal = new planner_astar.MultipleGoals();
-        var graph = new astar.Graph(new planner_astar.DijkstraHeuristic(), graphGoal);
-        var graphStart = new planner_astar.PlannerNode(state, null, null);
+        var graphGoal = new MultipleGoals();
+        var graph = new astar.Graph(new DijkstraHeuristic(), graphGoal);
+        var graphStart = new PlannerNode(state, null, null);
         var result = graph.searchPath(graphStart);
 
         for (var i = 1; i < result.path.length; i++) {
-            var current = <planner_astar.PlannerNode> result.path[i];
+            var current = <PlannerNode> result.path[i];
             plan.push(current.actionMessage);
             plan.push(current.lastAction);
         }
@@ -108,4 +109,98 @@ module Planner {
         return Math.floor(Math.random() * max);
     }
 
+    export class PlannerNode implements astar.INode {
+        state: WorldState;
+        lastAction: string; //l, r , d or p
+        actionMessage: string;
+
+        constructor(state, lastAction, actionMessage) {
+            this.state = state;
+            this.lastAction = lastAction;
+            this.actionMessage = actionMessage;
+        }
+
+        getUniqueId(): string {
+            return JSON.stringify(this.state);
+        }
+
+        getNeighbors(): astar.Neighbor[] {
+            var n: astar.Neighbor[] = [];
+
+            var useArmState = this.useArm();
+            if (useArmState) {
+                n.push(
+                    new astar.Neighbor(useArmState, 1));
+            }
+
+            var moveLeftState = this.moveArm(-1);
+            if (moveLeftState) {
+                n.push(
+                    new astar.Neighbor(moveLeftState, 1));
+            }
+
+            var moveRightState = this.moveArm(1);
+            if (moveRightState) {
+                n.push(
+                    new astar.Neighbor(moveRightState, 1));
+            }
+            return n;
+        }
+
+        useArm(): PlannerNode {
+            if (this.state.holding === null) {
+                console.log(this.state.holding);
+                console.log("picking object up");
+                var currentStack = this.state.stacks[this.state.arm];
+                if (currentStack.length > 0) {
+                    var topItemIndex = currentStack.length - 1;
+                    var newState = owl.deepCopy(this.state, 5);
+                    newState.holding = currentStack[topItemIndex];
+                    newState.stacks[this.state.arm].splice(topItemIndex, 1);
+                    var newMessage = "Picking up the " + newState.objects[newState.holding].form;
+                    return new PlannerNode(newState, "p", newMessage);
+                }
+            } else { // holding something at the moment
+                console.log("putting object down");
+                var newState = owl.deepCopy(this.state, 5);
+                //TODO: check if legal move
+                newState.stacks[newState.arm].push(newState.holding);
+                var newMessage = "Dropping the " + newState.objects[newState.holding].form;
+                newState.holding = null;
+                return new PlannerNode(newState, "d", newMessage);
+            }
+            return null;
+        }
+
+        moveArm(direction: number): PlannerNode {
+            var numberOfStacks = this.state.stacks.length;
+            var targetPos = this.state.arm + direction;
+            if (targetPos >= 0 && targetPos < numberOfStacks) {
+                var newState = owl.deepCopy(this.state, 5);
+                newState.arm = targetPos;
+                var newMessage = "Moving " + (direction > 0 ? "right" : "left");
+                return new PlannerNode(newState, direction > 0 ? "r" : "l", newMessage);
+            }
+            return null;
+        }
+    }
+
+    export class MultipleGoals implements astar.IGoal {
+
+        constructor() {
+        }
+
+        isReached(node: astar.INode): boolean {
+            var n = <PlannerNode> node;
+
+            //return n.state.arm === 2;
+            return n.state.stacks[1].length === 0;
+        }
+    }
+
+    export class DijkstraHeuristic implements astar.IHeuristic {
+        get(node: astar.INode, goal: astar.IGoal): number {
+            return 0;
+        }
+    }
 }
