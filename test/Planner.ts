eb2e5e7341e34b9dd1f7958bdb4355d5b2a1bccf
Lukas Beeck <meisterasrael@googelmail.com>
2015-05-11 13:04:45 +0200

    implemented expand function

--- test/Planner.ts
+++ test/Planner.ts
@@ -9,17 +9,17 @@ import C = require('../lib/collections');
 
 
 module PlannerTest {
-	class WorldState implements A.Astar.State {
+  class WorldState implements A.Astar.State {
     h: number;
     stacks: [[WorldObject]];
     crane: WorldObject;
-
+    
     match(goal: WorldState) {
-      for (var i = 0; i < stacks.length; i++) {
-        for (var j = 0; j < stacks[i].length; j++) {
-           if (stacks[i][j].form != goal.stacks[i][j].form
-               || stacks[i][j].size != goal.stacks[i][j].size
-               || stacks[i][j].color != goal.stacks[i][j].color)
+      for (var i = 0; i < this.stacks.length; i++) {
+        for (var j = 0; j < this.stacks[i].length; j++) {
+           if (this.stacks[i][j].form != goal.stacks[i][j].form
+               || this.stacks[i][j].size != goal.stacks[i][j].size
+               || this.stacks[i][j].color != goal.stacks[i][j].color)
              return false;
         }
       }
@@ -31,50 +31,56 @@ module PlannerTest {
     }
     
     expand() {
-      neighbours: [WorldState];
-      if (crane == null) {
-        for (var i = 0; i < stacks.length; i++) {
-          if (stacks[i].length == 0)
+      var neighbours;//: [Transition];
+      if (this.crane == null) {
+        for (var i = 0; i < this.stacks.length; i++) {
+          if (this.stacks[i].length == 0)
             continue;
-          topObject : WorldObject = this.stacks[i][this.stacks[i].length - 1];
+          var topObject : WorldObject = this.stacks[i][this.stacks[i].length - 1];
           
-          newWorld : WorldState = this.clone();
+          var newWorld : WorldState = this.clone();
           newWorld.stacks[i].splice(this.stacks[i].length - 1, 1); 
           newWorld.crane = topObject;
-          neighbours.push(newWorld);
+          neighbours.push({costs: 1, state: newWorld});
         }
       } else {
-        for (var i = 0; i < stacks.length; i++) {
-          newWorld : WorldState = this.clone();
+        for (var i = 0; i < this.stacks.length; i++) {
+          var newWorld : WorldState = this.clone();
           newWorld.stacks[i].push(this.crane);
           newWorld.crane = null;
-          neighbours.push(newWorld);
+          neighbours.push({costs: 1, state: newWorld});
         }
       }
       return neighbours;
     }
 
     toString() {
-      return stacks + " " + crane;
+      return this.stacks + " " + this.crane;
     }
 
-    constructor() {
-      this.crane = null;
-      this.h = 0;
-    }
-
-    constructor(stacks: [[String]], crane: String) {
-      for (var i = 0; i < stacks.length; i++) {
-        for (var j = 0; j < stacks[i].length; j++) {
-          this.stacks[i].push(new WorldObject(exampleWorldDescription[stacks[i][j]]));
+    constructor(stacks: [[string]], crane: string) {
+      if( stacks == null && crane == null){
+        this.crane = null;
+        this.h = 0;
+      } else if (stacks != null){
+        for (var i = 0; i < stacks.length; i++) {
+          for (var j = 0; j < stacks[i].length; j++) {
+            this.stacks[i].push(exampleWorldDescription.getValue(stacks[i][j]));
+          }
         }
+        if(crane != null){
+          this.crane = exampleWorldDescription.getValue(crane);
+        } else {
+          this.crane = null;
+        }
+        this.h = 0;
+      } else {
+        throw new Error("You are stupid! You cant create a world without a stack but with a crane!");
       }
-      this.crane = new WorldObject(crane);
-      this.h = 0;
     }
 
     clone() : WorldState {
-      braveNewWorld : WorldState = new WorldState();
+      var braveNewWorld : WorldState = new WorldState(null, null);
       for (var i = 0; i < this.stacks.length; i++) {
         for (var j = 0; j < this.stacks[i].length; j++) {
           braveNewWorld.stacks[i].push(this.stacks[i][j]);
@@ -125,47 +131,48 @@ module PlannerTest {
 
   function checkIfValid(state) {
     for (var i = 0; i < state.stacks.length; i++) {
-    for (var j = 1; j < state.stacks[i].length; j++) {
-    var currentObjectDescription = exampleWorldDescription.getValue(state.stacks[i][j]);
-    var belowObjectDescription = exampleWorldDescription.getValue(state.stacks[i][j-1]);
-    // balls must be in boxes or on the floor, otherwise they roll away
-    if (currentObjectDescription.form == "ball"
-      && belowObjectDescription.form != "box") 
-      return false;
+      for (var j = 1; j < state.stacks[i].length; j++) {
+        var currentObjectDescription = exampleWorldDescription.getValue(state.stacks[i][j]);
+        var belowObjectDescription = exampleWorldDescription.getValue(state.stacks[i][j-1]);
+        // balls must be in boxes or on the floor, otherwise they roll away
+        if (currentObjectDescription.form == "ball"
+            && belowObjectDescription.form != "box") 
+          return false;
 
-    // balls cannot support anything
-    if (belowObjectDescription.form == "ball")
-      return false;
+        // balls cannot support anything
+        if (belowObjectDescription.form == "ball")
+          return false;
 
-    // small objects cannot support large objects
-		if (currentObjectDescription.size == "large"
-      && belowObjectDescription.size == "small")
-      return false;
+        // small objects cannot support large objects
+	if (currentObjectDescription.size == "large"
+            && belowObjectDescription.size == "small")
+          return false;
 
-    // boxes cannot contain pyramids, planks or boxes of the same size
-    if ((currentObjectDescription.form == "pyramid"
-      || currentObjectDescription.form == "plank"
-      || currentObjectDescription.form == "boxes")
-      && belowObjectDescription.form == "box"
-      && belowObjectDescription.size == currentObjectDescription.size)
-      return false;
+        // boxes cannot contain pyramids, planks or boxes of the same size
+        if ((currentObjectDescription.form == "pyramid"
+             || currentObjectDescription.form == "plank"
+             || currentObjectDescription.form == "boxes")
+            && belowObjectDescription.form == "box"
+            && belowObjectDescription.size == currentObjectDescription.size)
+          return false;
 
-    // small boxes cannot be supported by small bricks or pyramids
-    if (currentObjectDescription.form == "box"
-      && currentObjectDescription.size == "small"
-      && belowObjectDescription.size == "small"
-      && (belowObjectDescription.form == "brick" || belowObjectDescription.form == "pyramid"))
-      return false;
+        // small boxes cannot be supported by small bricks or pyramids
+        if (currentObjectDescription.form == "box"
+            && currentObjectDescription.size == "small"
+            && belowObjectDescription.size == "small"
+            && (belowObjectDescription.form == "brick" || belowObjectDescription.form == "pyramid"))
+          return false;
 
-    // large boxes cannot be supported by large pyramids
-    if (currentObjectDescription.form == "box"
-      && currentObjectDescription.size == "large"
-      && belowObjectDescription.form == "pyramid"
-      && belowObjectDescription.size == "large")
-      return false;
+        // large boxes cannot be supported by large pyramids
+        if (currentObjectDescription.form == "box"
+            && currentObjectDescription.size == "large"
+            && belowObjectDescription.form == "pyramid"
+            && belowObjectDescription.size == "large")
+          return false;
+      }
     }
+    return true;
   }
-  return true;
 
   describe('Planner', () => {
 
@@ -191,5 +198,18 @@ module PlannerTest {
         done();
       });
     });
+    
+    describe('AStar in the Planner', () => {
+      it('test if a-star runs', (done) => {
+        var nothing: string = null;
+        var state: WorldState = new WorldState([[nothing],["g","l"],["e","f"],["k","m","f"],[nothing]], null);
+        var goal: WorldState = new WorldState([[nothing],["l","g"],["e","f"],["k","m","f"],[nothing]], null);
+        var solution = A.Astar.search(state, null, goal);
+        done()
+      })
+    }
+
+
+)
   });
 }
