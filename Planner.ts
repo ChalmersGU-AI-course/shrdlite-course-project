f3e41b33c574a2eaed8efed905b2b689bc57d3b7
axhav <william_axhav@hotmail.com>
2015-05-11 14:56:06 +0200

    planner work continued

--- Planner.ts
+++ Planner.ts
@@ -1,5 +1,6 @@
 ///<reference path="World.ts"/>
 ///<reference path="Interpreter.ts"/>
+///<reference path="AStar.ts"/>
 
 module Planner {
 
@@ -43,34 +44,76 @@ module Planner {
 	// state changing functions
 	function moveRight(world: WorldState) : WorldState
 	{
-		world.arm = world.arm+1;
-		return world;
+		return {stacks:world.stacks,holding:world.holding, arm:world.arm+1,objects:world.objects,examples:world.examples};
 	}
-	
 	function moveLeft(world: WorldState) : WorldState
 	{
-		world.arm = world.arm-1;
-		return world;
+		return {stacks:world.stacks,holding:world.holding, arm:world.arm-1,objects:world.objects,examples:world.examples};
 	}
 	function pickup(world:WorldState) : WorldState
 	{
-		world.holding = world.stacks[world.arm][world.stacks[world.arm].length-1];
-		world.stacks[world.arm].pop();
-		return world;
+		var arr : string[][] = world.stacks.slice();
+		for (var i = 0 ; i < world.stacks.length; i++)
+		{
+			arr[i] = world.stacks[i].slice();
+		}
+		var hold = arr[world.arm].pop();
+		
+		return {stacks:arr,holding:hold,arm:world.arm,objects:world.objects,examples:world.examples};
 	}
 	function putdown(world:WorldState) : WorldState
 	{
-		world.stacks[world.arm].push(world.holding);
-		world.holding = "";
+		var arr = world.stacks.slice();
+		for (var i = 0 ; i < world.stacks.length; i++)
+		{
+			arr[i] = world.stacks[i].slice();
+		}
+		arr[world.arm].push(world.holding);
 		
-		return world;
+		return {stacks:arr, holding:"",arm:world.arm,objects:world.objects,examples:world.examples};
 	}
 	
+	function worldItteration(world: WorldState) : [WorldState]
+	{
+		if(world.arm == 0)
+		{
+			if(world.holding != null)
+			{
+				return [moveRight(world),putdown(world)]
+			}
+			else
+			{
+				return [moveRight(world),pickup(world)]
+			}
+		}
+		if(world.arm == world.stacks.length-1)
+		{
+			if(world.holding != null)
+			{
+				return [moveLeft(world),putdown(world)]
+			}
+			else
+			{
+				return [moveLeft(world),pickup(world)]
+			}
+		}
+		if (world.holding != null)
+		{
+			return [moveRight(world),moveLeft(world),putdown(world)]
+		}
+		else
+		{
+			return [moveRight(world),moveLeft(world),pickup(world)]
+		}
+		
+	}
 	
 	
     function planInterpretation(intprt : Interpreter.Literal[][], state : WorldState) : string[] {
         // This function returns a dummy plan involving a random stack
-        do {
+        		
+		console.log(worldItteration(state), state);
+		do {
             var pickstack = getRandomInt(state.stacks.length);
         } while (state.stacks[pickstack].length == 0);
         var plan : string[] = [];
