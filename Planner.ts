794529a7b46235639dc54f90274871563001acab
Daniel <d.linden88@gmail.com>
2015-05-11 11:05:00 +0200

    add WorldNode with neighbours function to Planner

--- Planner.ts
+++ Planner.ts
@@ -1,5 +1,6 @@
 ///<reference path="World.ts"/>
 ///<reference path="Interpreter.ts"/>
+///<reference path="Astar.ts"/>
 
 module Planner {
 
@@ -38,6 +39,122 @@ module Planner {
 
     //////////////////////////////////////////////////////////////////////
     // private functions
+
+    class WorldNode implements INode<WorldNode> {
+        constructor(
+            public State: WorldState
+            ) {}
+        
+        Neighbours() : Neighbour<WorldNode>[] {
+            var neighbours: Neighbour<WorldNode>[] = [];
+
+            if (this.CanDrop()) {
+                neighbours.push(this.Drop());
+            }
+
+            if (this.CanPickUp()) {
+                neighbours.push(this.PickUp());
+            }
+
+            if (this.CanGoLeft()) {
+                neighbours.push(this.GoLeft());
+            }
+
+            if (this.CanGoRight()) {
+                neighbours.push(this.GoRight());
+            }
+            return neighbours;
+        }
+
+        CanDrop(): boolean {
+            if (!this.State.holding) {
+                return false;
+            }
+            var topObject = this.GetTopObjectInColumn(this.State.arm);
+            var heldObject = this.State.objects[this.State.holding];
+            return this.CanPutObjectOntop(heldObject, topObject);
+        }
+
+        CanGoLeft(): boolean {
+            return this.State.arm > 0;
+        }
+
+        CanGoRight(): boolean {
+            return this.State.arm < this.State.stacks.length - 1;
+        }
+
+        CanPickUp(): boolean {
+            if (this.State.holding) {
+                return false;
+            }
+            var topObject = this.GetTopObjectInColumn(this.State.arm);
+            return (topObject != null);
+        }
+
+        Drop(): Neighbour<WorldNode> {
+            var newStacks = this.CopyStacks();
+            newStacks[this.State.arm].push(this.State.holding);
+            var newWorld = this.Copy(newStacks);
+            newWorld.State.holding = null;
+            return new Neighbour<WorldNode>(newWorld, 1, "d");
+        }
+
+        GoLeft(): Neighbour<WorldNode> {
+            var newWorld = this.Copy(this.State.stacks);
+            newWorld.State.arm -= 1;
+            return new Neighbour<WorldNode>(newWorld, 1, "l");
+        }
+
+        GoRight(): Neighbour<WorldNode> {
+            var newWorld = this.Copy(this.State.stacks);
+            newWorld.State.arm += 1;
+            return new Neighbour<WorldNode>(newWorld, 1, "r");
+        }
+
+        PickUp(): Neighbour<WorldNode> {
+            var newStacks = this.CopyStacks();
+            var holding = newStacks[this.State.arm].pop();
+            var newWorld = this.Copy(newStacks);
+            newWorld.State.holding = holding;
+            return new Neighbour<WorldNode>(newWorld, 1, "p");
+        }
+
+        CopyStacks(): string[][] {
+            return this.State.stacks.map(function(stack) {
+                return stack.slice();
+                });
+        }
+
+        Copy(stacks: string[][]): WorldNode {
+            var state: WorldState = {
+                stacks: stacks,
+                holding: this.State.holding,
+                arm: this.State.arm,
+                objects: this.State.objects,
+                examples: this.State.examples,
+            };
+
+            return new WorldNode(state);
+        }
+
+        GetTopObjectInColumn(column: number): ObjectDefinition {
+            var stack = this.State.stacks[column];
+
+            for (var i = stack.length - 1; i >= 0; --i) {
+                if (stack[i]) {
+                    return this.State.objects[i];
+                }
+            }
+            return null;
+        }
+
+        CanPutObjectOntop(object: ObjectDefinition, baseObject: ObjectDefinition): boolean {
+            if (baseObject.form == "circle") {
+                return false;
+            }
+            return true;
+        }
+    }
 
     function planInterpretation(intprt : Interpreter.Literal[][], state : WorldState) : string[] {
         // This function returns a dummy plan involving a random stack
