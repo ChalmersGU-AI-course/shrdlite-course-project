abbc3948286682511079d8196d28da0d1dd3c894
axhav <william_axhav@hotmail.com>
2015-05-11 11:05:34 +0200

    parser sortof working

--- Interpreter.ts
+++ Interpreter.ts
@@ -11,8 +11,10 @@ module Interpreter {
         parses.forEach((parseresult) => {
             var intprt : Result = <Result>parseresult;
             intprt.intp = interpretCommand(intprt.prs, currentState);
-            interpretations.push(intprt);
+			if(intprt.intp !== null)
+				interpretations.push(intprt);
         });
+		
         if (interpretations.length) {
             return interpretations;
         } else {
@@ -56,8 +58,14 @@ module Interpreter {
         //console.log(cmd.ent.obj);
         
         //traverseObj(cmd,state,objs,compareWithWorld);
-        
-        var result :Literal[][] = traverseCommand(cmd,state,objs,compareWithWorld);
+        //console.log(state);
+        //console.log("------------------------------------");
+        var result :Literal[][] = interpretCmd(cmd,state,objs,compareWithWorld);
+		//console.log(result);
+		if(result.length <= 0)
+		{
+			return null;
+		}
         /*var failureVar : number = 0;
         for(var i in result)
         {
@@ -85,41 +93,75 @@ module Interpreter {
         return result;//intprt;
     }
     
-    function traverseCommand(cmd : Parser.Command, world : WorldState, wObjs : string[] , func : (obj: Parser.Object, world : WorldState, wObjs : string[]) => any ): Literal[][]
+    function interpretCmd(cmd : Parser.Command, world : WorldState, wObjs : string[] , func : (obj: Parser.Object, world : WorldState, wObjs : string[]) => any ): Literal[][]
     {
-        var res : Literal[][] = [[]];
+        var res : Literal[][] = [];
         //var lit :Literal = {pol:true,rel : cmd.loc.rel}
+        var result : string[]; 
         switch(cmd.cmd)
         {
             case "move":
-                if(typeof(cmd.ent) !== "undefined")
+                result = interpretEnt(cmd.ent,world,wObjs);
+                //console.log("after interpret ent ",result);
+                if(result.length <= 0)
                 {
-                    var ret : string[] = traverseEnt(cmd.ent,world,wObjs,func);
-                    var arg : string[] = [];
-                    for(var i = 1;i < ret.length; i++)
-                    {
-                        arg.push(ret[i]);
-                    }
-                    res[0].push( {pol:true,rel:ret[0],args:arg});
-                    //console.log("after traverse ent res= ", res);
+                    return [];
                 }
-                if(typeof(cmd.loc) !== "undefined")
+                var moveTo: string[] = interpretLoc(cmd.loc,world,wObjs);
+                console.log("after interloc" , moveTo);
+                if(moveTo.length <= 1)
                 {
-                    var ret : string[] = traverseEnt(cmd.loc.ent,world,wObjs,func)
-                    console.log("dfdfdfdf ", ret);
-                    var arg : string[] = [];
-                    arg.push(res[0][res.length-1].args[res[0][res.length-1].args.length -1]);
-                    arg.push(ret[0]);
-                    res[0] = res[0].concat({pol:true,rel:cmd.loc.rel,args:arg});
+                   return [];
                 }
+				for(var i = 1; i < moveTo.length; i++)
+				{
+					res.push([{pol:true,rel:moveTo[0],args:[result[0],moveTo[i]]}]);
+				}
+                
                 break;
             case "take":
-                    res[0].push({pol:true,rel:"holding",args:traverseEnt(cmd.ent,world,wObjs,func)});
+                    var result : string[] = interpretEnt(cmd.ent,world,wObjs);
+                    
+                    //res[0].push({pol:true,rel:"holding",args:res});
                 break;
         }
         //console.log("End of travCoom ", res);
+        //console.log(res);
         return res;
     }
+    
+    function interpretEnt(ent : Parser.Entity, world : WorldState, wObjs : string[]) : string[]
+    {
+        var res: string[] = [];
+        
+        if(typeof(ent.quant) !== "undefined")
+        {
+            //hantera på något sätt
+        }
+        
+        if (typeof (ent.obj) !== "undefined")
+        {
+            res = res.concat(compareWithWorld(ent.obj,world,wObjs));
+        }
+        return res; 
+    }
+    
+    function interpretLoc(loc: Parser.Location, world : WorldState, wObjs : string[]): string []
+    {
+        var res : string[] = [];
+        if (typeof(loc.rel) !== "undefined")
+        {
+            res.push(loc.rel);
+        }
+        
+        if (typeof(loc.ent) !== "undefined")
+        {
+            res = res.concat(interpretEnt(loc.ent,world,wObjs));
+        }
+        
+        return res;
+    }
+    
     
     function traverseEnt(ent : Parser.Entity, world : WorldState, wObjs : string[] , func : (obj: Parser.Object, world : WorldState, wObjs : string[]) => any ): string[]
     {
@@ -155,14 +197,34 @@ module Interpreter {
         return res;
     }
     
-    
-    function compareWithWorld(obj : Parser.Object, state : WorldState, wObjs : string[]) : string
+    function compareWithWorld(inobj : Parser.Object, state : WorldState, wObjs : string[]) : string[]
     {
         //console.log("searching for ", obj);
-        var res : string = "";
-        if(obj.form == 'floor')
+        var obj : Parser.Object;
+        var res : string[] =[];
+        var otherObjs : string[];
+        
+        if(inobj.form == 'floor')
         {
-            return "floor";
+			for(var i in state.stacks)
+			{
+				if(state.stacks[i].length <= 0 )
+				{
+					res.push("floor-"+i);
+				}
+			}
+			return res;
+        }
+        if(typeof(inobj.loc) !== "undefined")
+        {
+            otherObjs = interpretLoc(inobj.loc, state,wObjs);
+            obj= inobj.obj;
+            
+            //console.log("loc is undefined", obj);
+        }
+        else
+        {
+            obj = inobj;
         }
         wObjs.forEach((object) => {
             var temp : ObjectDefinition = state.objects[object];
@@ -173,7 +235,89 @@ module Interpreter {
                 {//console.log(temp.color + "    " + cmd.obj.color);
                     if(temp.color == obj.color || obj.color == null )
                     {
-                        res = object;
+                        if(otherObjs == null)
+                        {
+                            res.push(object);
+                        }
+                        else
+                        {
+                            //console.log("inside the loop ", object, "otherObjs are", otherObjs);
+                            for(var i in state.stacks)
+                            {
+                                for(var j in state.stacks[i])
+                                {
+                                    if(state.stacks[i][j] == object)
+                                    {
+                                        switch(otherObjs[0])
+                                        {
+                                            case "rightof":
+                                                for(var m : number = 0; m < i; m++)
+                                                {
+                                                    if(isInColumn(object,m,state))
+                                                    {
+                                                        res.push(object);
+                                                    }
+                                                }
+                                                break;
+                                            case "leftof":
+                                                for(var m : number = i+1; m < state.stacks.length; m++)
+                                                {
+                                                    if(isInColumn(object,i+1,state))
+                                                    {
+                                                        res.push(object);
+                                                    }
+                                                }
+                                                break;
+                                            case 'inside':
+                                                otherObjs.forEach((obj2) =>{
+                                                    if(state.stacks[i][j-1] == obj2)
+                                                    res.push(object);
+                                                });
+                                                break;
+                                            case "ontop":
+                                                otherObjs.forEach((obj2) =>{
+                                                    if(state.stacks[i][j+1] == obj2)
+                                                    res.push(object);
+                                                });
+                                                break;
+                                            case "under":
+                                                for(var m : number = 0; m < j; m++)
+                                                {
+                                                    otherObjs.forEach((obj2) =>{
+                                                        if(state.stacks[i][m] == obj2)
+                                                        res.push(object);
+                                                    });
+                                                }
+                                                break;    
+                                            case "beside":
+                                                otherObjs.forEach((obj2) =>{
+                                                    if(state.stacks[i][j-1] == obj2)
+                                                    res.push(object);
+                                                });
+                                                otherObjs.forEach((obj2) =>{
+                                                    if(state.stacks[i][j+1] == obj2)
+                                                    res.push(object);
+                                                });
+                                                break; 
+                                            case "above":
+                                                for(var m : number = j+1; m < state.stacks[i].length; m++)
+                                                {
+                                                    otherObjs.forEach((obj2) =>{
+                                                        if(state.stacks[i][m] == obj2)
+                                                        res.push(object);
+                                                    });
+                                                }
+                                                break;
+                                        }
+                                    }
+                                }
+                            }
+                            if(i != 0)
+                            {
+                                return 0;
+                            }
+                        
+                        }
                         //console.log("found ", object);
                         return res;
                     }
@@ -183,6 +327,19 @@ module Interpreter {
         //console.log("from func " +res);
         return res
     }
+    function isInColumn(obj: string, col: number, state:WorldState) : boolean
+    {
+        var result : boolean = false;
+        for (var i in  state.stacks[col]) 
+        {
+            if(obj == state.stacks[col][i])
+            {
+                result = true;
+            }
+        }
+        return result; 
+    }
+    
     
     function getRandomInt(max) {
         return Math.floor(Math.random() * max);
