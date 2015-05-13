0356b64498516a2bd69b267a3568833a6f825676
oskarssd <gusoskada@student.gu.se>
2015-05-13 13:58:04 +0200

    Specification for the commands

--- Interpreter.ts
+++ Interpreter.ts
@@ -7,10 +7,10 @@ module Interpreter {
     //////////////////////////////////////////////////////////////////////
     // exported functions, classes and interfaces/types
 
-    export function interpret(parses : Parser.Result[], currentState : WorldState) : Result[] {
-        var interpretations : Result[] = [];
+    export function interpret(parses: Parser.Result[], currentState: WorldState): Result[] {
+        var interpretations: Result[] = [];
         parses.forEach((parseresult) => {
-            var intprt : Result = <Result>parseresult;
+            var intprt: Result = <Result>parseresult;
             intprt.intp = interpretCommand(intprt.prs, currentState);
             if (intprt.intp !== null) {
                 interpretations.push(intprt);
@@ -24,110 +24,155 @@ module Interpreter {
     }
 
 
-    export interface Result extends Parser.Result {intp:Literal[][];}
-    export interface Literal {pol:boolean; rel:string; args:string[];}
+    export interface Result extends Parser.Result { intp: Literal[][]; }
+    export interface Literal { pol: boolean; rel: string; args: string[]; }
 
 
-    export function interpretationToString(res : Result) : string {
+    export function interpretationToString(res: Result): string {
         return res.intp.map((lits) => {
             return lits.map((lit) => literalToString(lit)).join(" & ");
         }).join(" | ");
     }
 
-    export function literalToString(lit : Literal) : string {
+    export function literalToString(lit: Literal): string {
         return (lit.pol ? "" : "-") + lit.rel + "(" + lit.args.join(",") + ")";
     }
 
 
     export class Error implements Error {
         public name = "Interpreter.Error";
-        constructor(public message? : string) {}
-        public toString() {return this.name + ": " + this.message}
+        constructor(public message?: string) { }
+        public toString() {return this.name + ": " + this.message }
     }
 
 
     //////////////////////////////////////////////////////////////////////
     // private functions
 
-    function interpretCommand(cmd : Parser.Command, state : WorldState) : Literal[][] {
-        
+    function interpretCommand(cmd: Parser.Command, state: WorldState): Literal[][] {
+
         // This function is called once for each parse found.
         // cmd is the command found for this particular parse
         // state should be the current WorldState
 
         var intprt: Literal[][] = [];
 
+        //Used for anything?
         // What quantity are we looking for? 0 = any, 1 = the, 2 = all
         var quant = -1
-        if(cmd.ent.quant == "the")
-          quant = 1;
+        if (cmd.ent.quant == "the")
+            quant = 1;
         else if (cmd.ent.quant == "any")
-          quant = 0;
+            quant = 0;
         else if (cmd.ent.quant == "all")
-          quant = 2;
-        
-        
+            quant = 2;
+
+
         // Get possible objects the parse is referring to
         var pobjs = getPossibleObjects(cmd, state);
         var b = pobjs[0];
+
+        /*
+            TODO: Do correct stuff with "take"
+                -Identify what object we want
+                -See if such an object exists in the world
+                -If ambiguity and the quantifier is 'the', ask for clarification
+        */
         if (cmd.cmd === "take") {
             if (cmd.ent.quant === "all") {
                 //Can't hold more than one object
                 //CHANGE IF ADDING ANOTHER ARM
                 console.log("Can't hold more than one object");
                 return null;
-            }
-            /*
-            TODO: Do correct stuff with "take"
-                -Identify what obj we want
-                -See if such an object exists in the world
-                -If ambiguity and the quantifier is 'the', ask for clarification //Om samma size, �nd� fr�ga?
-            */
-            if(pobjs.length===0){
+            } else if (cmd.ent.quant === "the" && pobjs.length > 1) {
+                console.log("Please be more specific");
+                //Possible extension to save the current data and ask a clarification question      <---TODO?
+                return null;
+            } else if (pobjs.length === 0) {
                 console.log("Can't pickup something that is not real");
                 return null;
             }
+
             for (var i = 0; i < pobjs.length; i++) {
                 intprt.push([{ pol: true, rel: "holding", args: [pobjs[i]] }]);
-            }    
+            }
         }
+
+        /*
+            TODO: Do correct stuff with "put"
+                -See if we hold an object o
+                -Identify the target object t (i.e. "floor")
+                -See if such an object exists in the world
+                -Check if the positioning is valid, (i.e. under(o, t))
+                -If ambiguity and the quantifier is 'the', ask for clarification
+        */
         else if (cmd.cmd === "put") {
             if (state.holding === null) {
                 //No knowledge of "it"
                 console.log("No knowledge of 'it'");
                 return null;
             }
-            //TODO: Do correct stuff with "put"
         }
+
+        /*
+            TODO: Do correct stuff with "move"
+                -Identify the primary objects o[]
+                -See if such an object exists in the world
+                -Identify the target objects t[] (i.e. "floor")
+                -See if such an object exists in the world
+                
+                -Handle quantifications:
+                    -   any -> the
+                            -If ambiguity for target, ask for clarification
+                    -   any -> any
+                    -   any -> all
+
+                    -   the -> the
+                            -If ambiguity for primary OR target, ask for clarification
+                    -   the -> any
+                            -If ambiguity for primary, ask for clarification
+                    -   the -> all
+                            -If ambiguity for primary, ask for clarification
+
+                    -   all -> the
+                            -If ambiguity for target, ask for clarification
+                    -   all -> any
+                    -   all -> all
+                -Handle relations:
+                    - //NEEDED?
+        
+                -If ambiguity and the quantifier is 'the', ask for clarification
+                -Check if the positioning is valid, ontop(o, t) 
+        */
         else if (cmd.cmd === "move") {
-            //TODO: Do correct stuff with "move"
+            //TODO
         } else {
             console.log("Found no valid command");
             return null;
         }
         return intprt;
 
-    /*
-        WONT REACH FURTHER DOWN, JUST KEEPING AS EXAMPLE
+        /*
+            WONT REACH FURTHER DOWN, JUST KEEPING AS EXAMPLE
 
-        // Dummy stuff
-        var objs : string[] = Array.prototype.concat.apply([], state.stacks);
-        var a = objs[2];
-        var b = objs[getRandomInt(objs.length)];
-        var intprt : Literal[][] = [[
-            {pol: true, rel: "ontop", args: [a, "floor"]},
-            {pol: true, rel: "holding", args: [b]}
-        ]];
-        return intprt;
-    */
+            // Dummy stuff
+            var objs : string[] = Array.prototype.concat.apply([], state.stacks);
+            var a = objs[2];
+            var b = objs[getRandomInt(objs.length)];
+            var intprt : Literal[][] = [[
+                {pol: true, rel: "ontop", args: [a, "floor"]},
+                {pol: true, rel: "holding", args: [b]}
+            ]];
+            return intprt;
+        */
     }
 
-    function getPossibleObjects(cmd : Parser.Command, state : WorldState){
+    function getPossibleObjects(cmd: Parser.Command, state: WorldState) {
         // Extract the descriptive parts of the object
         // By using a set we do not have to handle the null parts.
         // We could just check that the parsed object's set is a subset of 
         // the object from the stack
-     
+
         var objSet = new collections.Set<string>(); // Store the values of the object
         var o = cmd.ent.obj;
         objSet.add(o.size);
@@ -137,19 +182,19 @@ module Interpreter {
 
         var possibleObjects = [];
         // Loop through the world and look for possible items
-        var objs : string[] = Array.prototype.concat.apply([], state.stacks);
-        for(var s = 0; s < objs.length; s++){
-          var otemp = state.objects[objs[s]];
-          var stemp = new collections.Set<string>();
-        
-          // Extract the parts of o into s and check if objSet is subset of s.
-          stemp.add(otemp.form);
-          stemp.add(otemp.size);
-          stemp.add(otemp.color);
-        
-        // If the parse object is subset of the current temp object add to "possible objects"-array
-          if(objSet.isSubsetOf(stemp))
-            possibleObjects.push(objs[s]);   
+        var objs: string[] = Array.prototype.concat.apply([], state.stacks);
+        for (var s = 0; s < objs.length; s++) {
+            var otemp = state.objects[objs[s]];
+            var stemp = new collections.Set<string>();
+
+            // Extract the parts of o into s and check if objSet is subset of s.
+            stemp.add(otemp.form);
+            stemp.add(otemp.size);
+            stemp.add(otemp.color);
+
+            // If the parse object is subset of the current temp object add to "possible objects"-array
+            if (objSet.isSubsetOf(stemp))
+                possibleObjects.push(objs[s]);
         }
         return possibleObjects;
     }
@@ -159,4 +204,3 @@ module Interpreter {
     }
 
 }
-
