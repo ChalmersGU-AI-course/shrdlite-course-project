fb155550e7776a2290c3705848b31f37416166af
Jonatan <jonatan.kilhamn@gmail.com>
2015-05-18 11:25:02 +0200

    Add functionality of simple object descriptions
    The interpreter can now handle the verb part, as well as the object part if it'
    s simple. So "the red ball" works, but "the ball in the box" is not implemented yet. One form, or one form and optionally colour and/or size, is fine. "the floor" also works. Currently treating "any" and "the" the same, and rejecting everything else.

--- Interpreter.ts
+++ Interpreter.ts
@@ -50,7 +50,6 @@ module Interpreter {
     // private functions
 
     function interpretCommand(cmd : Parser.Command, state : WorldState) : Literal[][] {
-        // This returns a dummy interpretation involving two random objects in the world
         var objs : string[] = Array.prototype.concat.apply([], state.stacks);
 
         var verb : string = cmd.cmd;
@@ -65,7 +64,7 @@ module Interpreter {
         if (verb == "take") {
         // we shall pick up something and hold it;
             ent = cmd.ent;
-            object1 = findObjectFromEnt(ent);
+            object1 = findObjectFromEnt(state, ent);
             relation = "holding";
             objects = [object1];
        } else if (verb == "put") {
@@ -73,38 +72,85 @@ module Interpreter {
             object1 = whatAreWeHolding(state);
             // TODO: error handling if we are not actually holding anything
             loc = cmd.loc;
-            relation = findRelFromLoc(loc);
-            object2 = findObjFromLoc(loc);
+            relation = findRelFromLoc(state, loc);
+            object2 = findObjectFromLoc(state, loc);
             objects = [object1, object2]
        } else if (verb == "move") { 
        // we shall move something
             ent = cmd.ent;
-            object1 = findObjectFromEnt(ent);
+            object1 = findObjectFromEnt(state, ent);
             loc = cmd.loc;
-            relation = findRelFromLoc(loc);
-            object2 = findObjectFromLoc(loc);
+            relation = findRelFromLoc(state, loc);
+            object2 = findObjectFromLoc(state, loc);
             objects = [object1, object2]
         }
            
         var intprt : Literal[][];
         intprt = [[
-          {pol: true, rel: relation, args: args}
+          {pol: true, rel: relation, args: objects}
         ]];
         return intprt;
     }
     
+    
+    
     function findObjectFromEnt(state : WorldState, ent : Parser.Entity) : string {
-      var form = ent.form;
-      if (form == "floor") {
-        return form;
+      var obj : Parser.Object = ent.obj;
+      var quant : string = ent.quant;
+      var form : string = obj.form;
+      var color : string = obj.color;
+      var size : string = obj.size;
+      
+      if ((quant != "any") && (quant != "the")) {
+        return "fail";
+        //TODO: treat failure better. Exception?
+        //TODO: handle "any" differently from "the"
       }
-      //TODO: quantifiers
       
-      //TODO: simple case: object has colour and form
+      var candidates : string[];
       
-      //TODO: complex case: object is described in relation to another object
-      
+      if (form != null) {
+        // Simple case: size and colour are optional
+        if (form == "floor") {
+          return form;
+        } else {
+          candidates = getAllObjectsOfDesc(state, form, color, size);
+          return candidates[0];
+        }
+        
+      } else {
+        // Complex case: no size, colour or form; only another object and location
+        //TODO: complex case: object is described in relation to another object
+        
+
+      }
+
+   
       //TODO: error handling if object can't be found
+    }
+    
+    
+    
+    // Returns all objects in the world which fit a given (form, ?color, ?size) description
+    function getAllObjectsOfDesc(state : WorldState, form : string, color : string, size : string) : string[] {
+      var candidates : string[] = [];
+      state.stacks.forEach((stack) => {
+        stack.forEach((objectInStack) => {
+          var objDef : ObjectDefinition = state.objects[objectInStack];
+          if (objDef.form == form) {
+            console.log(form);
+            if (color == null || objDef.color == color) {
+              console.log(color);
+              if (size == null || objDef.size == size) {
+                console.log(size);
+                console.log(objectInStack);
+                candidates.push(objectInStack);
+              }
+            }
+          }
+        });
+      });
+      return candidates;
     }
 
     function findRelFromLoc(state : WorldState, loc : Parser.Location) : string {
