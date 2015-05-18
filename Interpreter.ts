4b34f6bc0ce70c4f24dcb9f188162f835dd84df7
Fredrik <fredrik.mikael.strom@gmail.com>
2015-05-18 19:56:50 +0200

    Interpreter, hope fully the all quantifier is working ok # Fredrik

--- Interpreter.ts
+++ Interpreter.ts
@@ -6,7 +6,8 @@ module Interpreter {
     //////////////////////////////////////////////////////////////////////
     // exported functions, classes and interfaces/types
 	var clairifyingparse :Parser.ResultAnswer[];
-    export function interpret(parses : Parser.Result[], clairifyparse:Parser.ResultAnswer[] , currentState : WorldState) : Result[] {
+    export function interpret(parses : Parser.Result[], clairifyparse:Parser.ResultAnswer[] , 
+    		currentState : WorldState) : Result[] {
         var interpretations : Result[] = [];
         clairifyingparse = clairifyparse;
         parses.forEach((parseresult) => {
@@ -14,7 +15,8 @@ module Interpreter {
         	if(intprt.prs.cmd){
     			intprt.intp = interpretCommand(intprt.prs, currentState);
 			}else{
-				throw new Interpreter.ErrorInput("This is a statement. \"" + intprt.input +"\" . Please tell me a command.");
+				throw new Interpreter.ErrorInput("This is a statement. \"" + 
+					intprt.input +"\" . Please tell me a command.");
 			}
 			if(intprt.intp.length){
             	interpretations.push(intprt);
@@ -83,75 +85,174 @@ module Interpreter {
         				n = 0;
         			}
         			var ob = state.objects[lstlits[l]];
-        			var object : Parser.Object = {obj: {obj:null, loc :null,size:ob.size, color: ob.color, form:ob.form}, 
+        			var object : Parser.Object = {obj: {
+        						obj:null, loc :null,size:ob.size, color: ob.color, form:ob.form}, 
         						loc : cmd.loc , size:ob.size, color: ob.color, form:ob.form};
         			var tempent : Parser.Entity = {quant:"any", obj: object};
         			temp = identifyEnt(tempent, null, lstlits[l], state);
         		//	tint = append(tint, temp);
-        			if(cmd.ent.quant == "all"){
+        			if(cmd.ent.quant == "all" || cmd.loc.ent.quant == "all"){
         				tint = merge(tint, temp);
         			}else{
         				tint = append(tint, temp);
         			}
-	        	/*	for(var j = 0; j < temp.length; j++){
-	        			var temp2 : Literal[]= clearIlligal(clone<Literal[]>(objs[i]), state);
-	        			
-	        			for(var k = 0; k < temp[j].length; k++){
-	        				if(checkIllegal(temp[j][k], state)){
-		        				intprt[n] = append(intprt[n], temp2);
-		        				intprt[n].push(temp[j][k]);
-		        				n++;
-	        				}
-	        			}
-	        		}*/
         		}
-        		var test = combineLiterals([],tint, objs[i], 0, 0, 0);
-    			for(var m = 0; m < test.length; m++){
-    				test[m] = clearIlligal(clone<Literal[]>(test[m]), state);
-    			}
-    			intprt = append(intprt,test);
+        		var ks : number[] = [0];
+        		var maxL = objs[i].length;
+        		if(tint[0]){
+        			if(tint[0].length > objs[i].length){
+        				maxL = tint[0].length;
+        			}
+        		}
         		
-        	}
+        		for(var j = 0; j < maxL; j++){
+        			ks.push(0);
+        		}
+        		var test = combineLiterals([],tint, objs[i], ks, cmd.loc.rel, state);
+        		var clearedlits : Literal[][]= [];
+    			for(var m = 0; m < test.length; m++){
+    				var lits = clearIlligal(clone<Literal[]>(test[m]), state);
+    				if(lits.length > 0){
+    					clearedlits.push(lits); 
+    				}
+    				
+    			}
+    			if(cmd.loc.ent.quant == "all"){
+    				if(intprt.length == 0){
+    					intprt = merge(intprt,clearedlits);
+    				}else{
+    					var tempintprt = combine(intprt,clearedlits, state); // combine instead
+    					var k = 0;
+    					for(var j = 0; j < tempintprt.length; j++){
+    						var clrdintprt : Literal[] =clearIlligal(tempintprt[j],state)
+    						if(clrdintprt.length != 0){
+    							intprt[k]= clrdintprt;
+    							k++;
+    						}
+    					}
+    				}
+    			}else{
+    				intprt = append(intprt,clearedlits);
+    			}
+           	}	
         }
         
         else if(cmd.cmd == "take"){
-        	intprt[0] =[{pol:true, rel:cmd.cmd, args : findendliterals(objs[0])}];
+        	for(var i = 0; i < objs.length; i++){
+        		for(var j = 0; j < objs[i].length; j++){
+        			objs[i][j].rel = "hold";
+        		}
+        		intprt.push(objs[i]);
+        	}
+        //	intprt[0] =[{pol:true, rel:cmd.cmd, args : findendliterals(objs[0])}];
         }
 
         return intprt;
     }
     
+    function combine(loclitss : Literal[][], objlitss : Literal[][], 
+    		state: WorldState):Literal[][]{
+    	var result : Literal[][]= [];	
+    	
+    	for(var i = 0; i < objlitss.length; i ++){
+    		var ks : number[] = [0];
+			var maxL = objlitss[i].length;
+			if(loclitss[0]){
+				if(loclitss[0].length > objlitss[i].length){
+					maxL = loclitss[0].length;
+				}
+			}
+			
+			for(var jj = 0; jj < maxL; jj++){
+				ks.push(0);
+			}
+    		append(result,combineLiterals([], loclitss, objlitss[i], ks, "", state));
+    	}		
+    	
+    	return result;
+    }
+    
     function combineLiterals(litss : Literal[][], loclitss : Literal[][]
-    			, objlits : Literal[], k : number, k2 :number , k3 : number):Literal[][]{
-    	// 1 x 5  = 5 
+    			, objlits : Literal[], ks : number[], rel : string, 
+    			state : WorldState):Literal[][]{
+    	var k = ks[0];
     	if(k == Math.pow(loclitss.length,  objlits.length)){
     		return litss;
     	}
-    //	for(var i = 0; i < objlits.length; i++){
-    		
+		if(!checkequal(ks,rel)){
 			litss[k] = append(litss[k], objlits);
-			litss[k] = append(litss[k], [loclitss[ (k2) % loclitss.length][0]]);
-			litss[k] = append(litss[k], [loclitss[ (k3) % loclitss.length][1]]);
-			if(k3 == loclitss.length-1){
-    			k3 = 0;
-    			k2 ++;
-    		}else{
-    			k3++;
-    		}
-    		
-			return combineLiterals(litss, loclitss, objlits, k+1, k2 ,k3);
+			var j = 0;
+			for(var i = 1; i < ks.length; i ++){
+				litss[k] = append(litss[k], [loclitss[ks[i]%loclitss.length][j]]);
+				j++;
+			}
+		}
+		var inced = false;
+		for(var i = ks.length-1; i > 0 ; i--){
+			if(ks[i]== loclitss.length-1){
+				ks[i] = 0;
+				if(i > 1){
+					ks[i-1] = ks[i-1] +1;
+				}
+			}else if(i == ks.length-1){
+				ks[i] = ks[i] +1;
+			}
+		}
 			
-  //  	}
+		ks[0] = k+1;
+		return combineLiterals(litss, loclitss, objlits, ks, rel, state);
+    }
+    
+    function checkequal(ks : number[], rel : string):boolean{
+    	if(ks.length == 1){
+    		return false;
+    	}
+    	for(var i = 1; i < ks.length; i++){
+    		for(var j = i+1; j < ks.length ; j++){
+    			if(ks[i]==ks[j] && i != j && (rel == "ontop" || rel == "inside")){
+    				return true;
+    			}
+    		}
+    	}
+    	return false;
+    }
+    
+    function checkillegalcombi(lits : Literal[]):boolean{
+    	for(var i = 0; i < lits.length; i ++){
+    		var templit = lits[i];
+    		for(var j = 0; j < lits.length; j++){
+	    		var templit2 = lits[j];
+	    		if(templit2.rel == templit.rel && i != j){
+	    			if((templit2.rel == "ontop" || templit2.rel == "inside") && 
+	    					(templit2.args[0] == templit.args[0] || 
+	    					 templit2.args[1] == templit.args[1] ||
+	    					 templit2.args[0] == templit.args[1] ||
+	    					 templit2.args[1] == templit.args[0])){
+	    				return true;
+	    			}
+	    		}
+    		}
+    	}
+    	
+    	return false;
     }
     
     function clearIlligal(lits : Literal[], state): Literal[]{
     	var cleared : Literal[] = [];
+    	if(!lits){
+    		return cleared;
+    	}
     	for(var i = 0; i < lits.length;i++){
     		if(checkIllegal(lits[i], state)){
     			cleared.push(lits[i]);
     		}
     	}
-    	return cleared;
+    	
+    	if(!checkillegalcombi(lits)){
+    		return cleared;
+    	}else{
+    		return [];
+    	}
     }
     
     function findendliterals(lits : Literal []): string[]{
@@ -175,14 +276,15 @@ module Interpreter {
     }
     
     
-    function checkQuantifyer(lits : Literal[], ent : Parser.Entity, loc : Parser.Location, state : WorldState):boolean{
+    function checkQuantifyer(lits : Literal[], ent : Parser.Entity, 
+    		loc : Parser.Location, state : WorldState):boolean{
     	if(ent.quant == "the"){
     		if(lits.length > 1){
     			return false;
     		}
     	}else if(ent.quant == "all" && loc.ent.quant != "all" ||
     				ent.quant != "all" && loc.ent.quant == "all"){
-    		var totalUnqObjs = findAllWithForm(ent.obj.form, state);
+    		var totalUnqObjs = findAllObjsWith(ent.obj.form, ent.obj.color, ent.obj.size, state);
     		if( (loc.rel != "beside")){
 	    		if(lits.length != totalUnqObjs.length){
 	    			return false;
@@ -200,27 +302,6 @@ module Interpreter {
     	return true;
     }
     
-    function checkIllegalCombi (lits : Literal[], litscomb :Literal [][], state : WorldState):boolean{
-		//return true;
-    	for(var i = 0; i < lits.length; i++){
-    		for(var j = i+1; j < lits.length; j++){
-    			if(lits[i].rel == lits[j].rel ){
-    				/*if(	(lits[i].args[0] == lits[j].args[0] ||		// remove dubletter
-	    					lits[i].args[1] == lits[j].args[1]) ){
-    					return false;
-    				}
-    				if(	(lits[i].args[0] == lits[j].args[1] ||		// remove dubletter
-	    					lits[i].args[1] == lits[j].args[0])){
-    					return false;
-    				}*/
-    				
-				}
-    		}
-    	}
-    	
-    	
-    	return true;
-    }
     
     function checkIllegal(lit : Literal, state : WorldState):boolean{
     	var a = state.objects[lit.args[0]];
@@ -248,11 +329,13 @@ module Interpreter {
     	if(lit.rel == "inside" && (b.form != "box")){
     		return false;
     	}
-    	if(lit.rel == "inside" && ((a.form == "pyramid" || a.form == "plank" || a.form == "floor" || a.form == "box")&&
+    	if(lit.rel == "inside" && ((a.form == "pyramid" || a.form == "plank" || 
+    			a.form == "floor" || a.form == "box")&&
     			(a.size == b.size || (a.size == "large" && b.size == "small")))){
     		return false;
     	}
-    	if((lit.rel == "ontop" || lit.rel == "above" || lit.rel == "inside") && ((a.size == "large" && b.size == "small")|| a.form == "floor")){
+    	if((lit.rel == "ontop" || lit.rel == "above" || lit.rel == "inside") && 
+    			((a.size == "large" && b.size == "small")|| a.form == "floor")){
     		return false;
     	}
     	if(lit.rel == "under" && ((b.size == "large" && a.size == "small") || a.form == "ball")){
@@ -285,19 +368,22 @@ module Interpreter {
     	var parseresult = clairifyingparse[clairifyingparse.length-1].prs.obj;
     	if(parseresult.form){
     		if(obj.form && obj.form != parseresult.form){
-    			throw new Interpreter.ErrorInput("Are we talking in terms of " + obj.form + " or " + parseresult.form +"? I would say " + obj.form + ".");
+    			throw new Interpreter.ErrorInput("Are we talking in terms of " + obj.form + 
+    				" or " + parseresult.form +"? I would say " + obj.form + ".");
     		}
     		obj.form = parseresult.form;
     	}
     	if(parseresult.color){
     		if(obj.color && obj.color != parseresult.color){
-    			throw new Interpreter.ErrorInput("You have already told me that the " + obj.form + " is " + obj.color +".");
+    			throw new Interpreter.ErrorInput("You have already told me that the " + 
+    				obj.form + " is " + obj.color +".");
     		}
     		obj.color = parseresult.color;
     	}
     	if(parseresult.size){
     		if(obj.size && obj.size != parseresult.size){
-    			throw new Interpreter.ErrorInput("You have already told me that the " + obj.form + " is " + obj.size+".");
+    			throw new Interpreter.ErrorInput("You have already told me that the " + 
+    				obj.form + " is " + obj.size+".");
     		}
     		obj.size = parseresult.size;
     	}
@@ -307,9 +393,6 @@ module Interpreter {
     }
     
     function identifyObj(form :string, color :string, size :string, state : WorldState):string[]{
-       //	var form = obj.form;
-      //  var color = obj.color;
-      //  var size = obj.size;
     	var objs : collections.Set<string> = new collections.Set<string>(function (x){return x});
         if(!form){
         	return [];
@@ -330,6 +413,9 @@ module Interpreter {
 	        	//check the first arg for form, color and size if it matches, add it to possibel objs
 	        	var a = state.objects[pddl.args[0]];
 	        	if(a.form != form && form != "anyform"){
+	        		continue;
+	        	}
+	        	if(a.form == "floor" && form == "anyform"){
 	        		continue;
 	        	}
 	        	if(!a){
@@ -353,7 +439,8 @@ module Interpreter {
     
     
     
-    function identifyEnt(ent : Parser.Entity, rel : string , obj : string ,state : WorldState):Literal[][]{
+    function identifyEnt(ent : Parser.Entity, rel : string , 
+    		obj : string ,state : WorldState):Literal[][]{
     	var result : string[];
     	if(obj == null && ent.obj){
 	    	if(ent.obj.loc && ent.obj.obj){
@@ -366,42 +453,46 @@ module Interpreter {
     	}
     	 
     	var unqObjs : string[] = uniqeObjects(result);
-    	var results : Literal[][] = [];
+    	var locresults : Literal[][] = [];
     	if(ent.obj.loc){
 
     		var locres : Literal [][]= identifyLocation(ent.obj.loc, state);
     		for(var i = 0; i < locres.length; i ++){
-    			//if()
-    			if(!results[i]){
-    				results[i] = [];
+    			if(!locresults[i]){
+    				locresults[i] = [];
     			}
-    			results[i] = append<Literal>(results[i], locres[i]);
-    		}
+				locresults[i] = append<Literal>(locresults[i], locres[i]);
+			}
     	}
     	
     	if(ent.quant == "the" && ent.obj.form != "floor"){
     		// ambigous interpet, use clairifying parse
     		if(unqObjs.length > 1){
 				if(!clairifyingparse){
-					throw new Interpreter.ErrorInput("Could you tell me which " + state.objects[result[0]].form + " I should move?");
+					throw new Interpreter.ErrorInput("Could you tell me which " + 
+						state.objects[result[0]].form + " I should move?");
 				}
-				var objs = solveAmbiguity(ent.obj,unqObjs, state);
+				var objs : string[]= solveAmbiguity(ent.obj,unqObjs, state);
 				if(objs.length > 1){
-					throw new Interpreter.ErrorInput("Could you tell me which " + state.objects[result[0]].form + " I should move?");
+					throw new Interpreter.ErrorInput("Could you tell me which " + 
+						state.objects[result[0]].form + " I should move?");
 				}
 				result = objs;
     		}
     		
     	} else
     	if(ent.quant == "all"){
-    		var totalUnqObjs = findAllWithForm(ent.obj.form, state);
+    		var totalUnqObjs = findAllObjsWith(ent.obj.form, ent.obj.color, 
+    			ent.obj.size, state);
     		if(unqObjs.length != totalUnqObjs.length){
     			if(!clairifyingparse){
-					throw new Interpreter.ErrorInput("Could you tell me which " + state.objects[result[0]].form + " I should move?");
+					throw new Interpreter.ErrorInput("Could you tell me which " + 
+						state.objects[result[0]].form + " I should move?");
 				}
-				var objs = solveAmbiguity(ent.obj,unqObjs, state);
+				var objs : string[] = solveAmbiguity(ent.obj,unqObjs, state);
 				if(objs.length > 1){
-					throw new Interpreter.ErrorInput("Could you tell me which " + state.objects[result[0]].form + " I should move?");
+					throw new Interpreter.ErrorInput("Could you tell me which " + 
+						state.objects[result[0]].form + " I should move?");
 				}
 				result = objs;
     		}
@@ -410,21 +501,87 @@ module Interpreter {
     	if (ent.quant == "any" || ent.obj.form == "floor"){ // any
    			;// TODO
     	}
+    	// Find location to move to
+        var intprt : Literal[][] = [];
+        var n : number = 0; 
+    	var temp : Literal [][] = [];
+    /*	for(var i = 0; i < result.length; i++){
+    		
+    		var tint : Literal[][] = [];
+    		if(ent.obj.loc){
+    		//	var lstlits : string[] = findendliterals(result[i]);
+	    		for(var l = 0; l < result.length; l++){
+	    			if(ent.quant == "all"){
+	    				n = 0;
+	    			}
+	    			var ob = state.objects[result[l]];
+	    			var object : Parser.Object = {obj: {
+	    						obj:null, loc :null,size:ob.size, color: ob.color, form:ob.form}, 
+	    						loc : ent.obj.loc , size:ob.size, color: ob.color, form:ob.form};
+	    			var tempent : Parser.Entity = {quant:"any", obj: object};
+	    			temp = identifyEnt(tempent, null, null, state);
+	    		//	tint = append(tint, temp);
+	    			if(ent.quant == "all"){
+	    				tint = merge(tint, temp);
+	    			}else{
+	    				tint = append(tint, temp);
+	    			}
+	    		}
+	    		var ks : number[] = [0];
+	    		var maxL = 0;
+	    		if(tint[0]){
+	    			if(tint[0].length > result[i].length){
+	    				maxL = tint[0].length;
+	    			}else{
+	    				maxL = result[i].length;
+	    			}
+	    		}
+	    		
+	    		for(var j = 0; j < maxL; j++){
+	    			ks.push(0);
+	    		}
+	    		var test = combineLiterals([],tint, locresults[i], ks, ent.obj.loc.rel);
+	    		var clearedlits : Literal[][]= [];
+				for(var m = 0; m < test.length; m++){
+					var lits = clearIlligal(clone<Literal[]>(test[m]), state);
+					if(lits.length > 0){
+						clearedlits.push(lits); 
+					}
+					
+				}
+				intprt = append(intprt,clearedlits);
+			}else{
+				if(ent.quant != "all"){
+					n = i;
+				}
+				
+				var lit = {pol : true, rel : rel, args : [result[i]]};
+				if(!intprt[n]){
+					intprt[n] = [];
+				}
+				intprt[n].push(lit);
+			}
+    	}*/
+        
+    	
     	// combine with possible locations
+    	
     	var intrpt : Literal[][] = [];
     	var n : number = 0; 
 		for(var i = 0; i < result.length; i++){
 			var lit : Literal;
 			if(ent.obj.loc){
-				for(var j = 0 ; j < results.length ;j++){
-					for(var k = 0 ; k < results[j].length ;k++){
-						lit = {pol : true, rel : ent.obj.loc.rel, args : [result[i], results[j][k].args[0]]};
+				for(var j = 0 ; j < locresults.length ;j++){
+					for(var k = 0 ; k < locresults[j].length ;k++){
+						// create new literal
+						lit = {pol : true, rel : ent.obj.loc.rel, args : [result[i], 
+								locresults[j][k].args[0]]};
 						if(checkIllegal(lit, state)){
 							if(!intrpt[n]){ 	// used j before
 								intrpt[n] = []; 
 							}
-							if(checkIllegal(results[j][k], state)){
-								intrpt[n].push(results[j][k]);
+							if(checkIllegal(locresults[j][k], state)){
+								intrpt[n].push(locresults[j][k]);
 							}
 							intrpt[n].push(lit);
 							n++;
@@ -446,8 +603,8 @@ module Interpreter {
     	return intrpt;
     }
     
-    function findAllWithForm(form : string, state : WorldState):string[]{
-    	var objs = identifyObj(form, "", "",state);
+    function findAllObjsWith(form : string, color : string, size : string, state : WorldState):string[]{
+    	var objs = identifyObj(form, color, size, state);
     	return uniqeObjects(objs);
     }
     
