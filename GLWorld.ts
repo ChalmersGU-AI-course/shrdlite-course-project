d10ce072fa3be1dd8f172e4e42ddc4a9a2831ad7
Markus Lindelöw <markus@lindeloew.se>
2015-05-20 01:39:36 +0200

    simple test

---
+++ GLWorld.ts
@@ -0,0 +1,150 @@
+///<reference path="World.ts"/>
+///<reference path="lib/jquery.d.ts" />
+///<reference path="lib/gl-matrix.d.ts" />>
+
+
+class GLGWorld implements World {
+    protected gl: WebGLRenderingContext;
+    protected enabled: boolean;
+    protected floorVertexBuffer: WebGLBuffer;
+    protected shaderProgram: WebGLProgram;
+    constructor(
+        public currentState: WorldState,
+        public canvas: HTMLCanvasElement
+        ) {
+        try {
+            this.gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+
+            
+            this.gl.clearColor(0.0, 0.0, 0.2, 1.0);
+            this.gl.enable(this.gl.DEPTH_TEST);
+            this.gl.depthFunc(this.gl.LEQUAL);
+
+            //Matrices
+            var mvMatrix: Float32Array = mat4.create();
+            var pMatrix: Float32Array = mat4.create();
+            
+
+            //Shader set-up
+            this.shaderProgram = this.gl.createProgram();
+
+            var vertexShaderSrc: string =
+                "attribute vec3 aVertexPosition;" +
+                "uniform mat4 uMVMatrix;" +
+                "uniform mat4 uPMatrix;" +
+                "void main(void) {" +
+                "gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);" +
+                "}";
+
+
+            var vertexShader: WebGLShader = this.gl.createShader(this.gl.VERTEX_SHADER);
+            this.gl.shaderSource(vertexShader, vertexShaderSrc);
+            this.gl.compileShader(vertexShader);
+            this.gl.attachShader(this.shaderProgram, vertexShader);
+
+            var fragmentShaderSrc: string =
+                "precision mediump float;" +
+                "void main(void) {" +
+                "gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);" +
+                "}";
+
+            var fragmentShader: WebGLShader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
+            this.gl.shaderSource(fragmentShader, fragmentShaderSrc);
+            this.gl.compileShader(fragmentShader);
+            this.gl.attachShader(this.shaderProgram, fragmentShader);
+
+
+            this.gl.linkProgram(this.shaderProgram);
+
+            if (!this.gl.getProgramParameter(this.shaderProgram, this.gl.LINK_STATUS)) {
+                alert("Could not initialise shaders");
+            }
+
+            this.gl.useProgram(this.shaderProgram);
+
+            var vertexPositionAttribute = this.gl.getAttribLocation(this.shaderProgram, "aVertexPosition");
+            this.gl.enableVertexAttribArray(vertexPositionAttribute);
+
+            var pMatrixUniform: WebGLUniformLocation = this.gl.getUniformLocation(this.shaderProgram, "uPMatrix");
+            var mvMatrixUniform: WebGLUniformLocation = this.gl.getUniformLocation(this.shaderProgram, "uMVMatrix");
+
+            //Set up some buffers
+            this.floorVertexBuffer = this.gl.createBuffer();
+            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.floorVertexBuffer);
+            var vertices = [
+                0.0, 1.0, 0.0,
+                -1.0, -1.0, 0.0,
+                1.0, -1.0, 0.0
+            ];
+
+            this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(vertices), this.gl.STATIC_DRAW);
+            
+
+
+
+
+            //Now draw!
+
+            this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+            this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+
+            mat4.perspective(pMatrix, 45, this.canvas.width / this.canvas.height, 0.1, 100.0);
+            mat4.identity(mvMatrix);
+            
+
+            var translation = vec3.create();
+            vec3.set(translation, -1.5, 0.0, -7.0);
+            mat4.translate(mvMatrix, mvMatrix, translation);
+
+
+            
+            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.floorVertexBuffer);
+            this.gl.vertexAttribPointer(vertexPositionAttribute, 3, this.gl.FLOAT, false, 0, 0);
+            this.gl.uniformMatrix4fv(pMatrixUniform, false, pMatrix);
+            this.gl.uniformMatrix4fv(mvMatrixUniform, false, mvMatrix);
+
+            this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 3);
+
+
+            
+            
+
+
+
+
+
+
+
+            
+
+        }
+        catch (e) {
+            alert(e);
+            this.gl = undefined;
+        }
+    }
+
+    printWorld(callback?: () => void): void {
+        //Guard flags
+        if (!this.gl)
+            return;
+
+
+        
+
+        //Draw floor
+
+
+
+    }
+    performPlan(plan: string[], callback?: () => void): void {
+    }
+    readUserInput(prompt: string, callback: (string) => void): void {
+    }
+    printSystemOutput(output: string, participant?: string): void {
+    }
+    printDebugInfo(info: string): void {
+    }
+    printError(error: string, message?: string): void {
+    }
+}
