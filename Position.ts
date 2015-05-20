0a26d1bd2a4915564aaa450a1172d7a3d929cdc5
Leif Schelin <schelinleif@gmail.com>
2015-05-20 12:10:56 +0200

    Fixed indentation

--- Position.ts
+++ Position.ts
@@ -93,52 +93,52 @@ function computeObjectPosition(s : State, objA : String) : ObjectPosition{
 }
 
 
-    /**
-    * Returns negative value if in different stacks.
-    * Otherwise, returns the number of objects in between.
-    * Is negative if b is above a.
-    */
-    function heightDifference(s : State, above : String, below : String) : number {
-        var a = computeObjectPosition(s, above);
-        var b = computeObjectPosition(s, below);
-        if(b.isHeld || a.isHeld){
-            return -1;
-        }
-
-        if(a.isFloor){
-            throw new Planner.Error("heightDiff: Floor cannot be above anything... "+
-                                    "Should never happen.");
-        }
-
-        if(b.isFloor){
-            // Dont touch this line!!! Becomes string concatenation otherwise...
-            return (+a.heightNo) + 1;
-        }
-
-        if(a.stackNo == b.stackNo){
-            return a.heightNo - b.heightNo;
-        }
-
+/**
+* Returns negative value if in different stacks.
+* Otherwise, returns the number of objects in between.
+* Is negative if b is above a.
+*/
+function heightDifference(s : State, above : String, below : String) : number {
+    var a = computeObjectPosition(s, above);
+    var b = computeObjectPosition(s, below);
+    if(b.isHeld || a.isHeld){
         return -1;
     }
 
-    /**
-    * Returns negative value if o2 is left of o1.
-    * Returns positive value if o2 is right of o1.
-    * Value is the difference in stacks (1 or -1 if in stacks beside each other, 0 if same stack).
-    */
-    function stackDifference(s : State, o1 : String, o2 : String) : number {
-        var a = computeObjectPosition(s, o1);
-        var b = computeObjectPosition(s, o2);
-        if(b.isHeld || a.isHeld){
-            return 0;
-        }
-
-        if(b.isFloor || a.isFloor){
-            // TODO Make sure that this never happens...
-            throw new Planner.Error("Floor should not be tested as beside anything?, at least not in this manner...");
-            return 0;
-        }
-
-        return a.stackNo - b.stackNo;
+    if(a.isFloor){
+        throw new Planner.Error("heightDiff: Floor cannot be above anything... "+
+                                "Should never happen.");
     }
+
+    if(b.isFloor){
+        // Dont touch this line!!! Becomes string concatenation otherwise...
+        return (+a.heightNo) + 1;
+    }
+
+    if(a.stackNo == b.stackNo){
+        return a.heightNo - b.heightNo;
+    }
+
+    return -1;
+}
+
+/**
+* Returns negative value if o2 is left of o1.
+* Returns positive value if o2 is right of o1.
+* Value is the difference in stacks (1 or -1 if in stacks beside each other, 0 if same stack).
+*/
+function stackDifference(s : State, o1 : String, o2 : String) : number {
+    var a = computeObjectPosition(s, o1);
+    var b = computeObjectPosition(s, o2);
+    if(b.isHeld || a.isHeld){
+        return 0;
+    }
+
+    if(b.isFloor || a.isFloor){
+        // TODO Make sure that this never happens...
+        throw new Planner.Error("Floor should not be tested as beside anything?, at least not in this manner...");
+        return 0;
+    }
+
+    return a.stackNo - b.stackNo;
+}
