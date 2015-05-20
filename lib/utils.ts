7fa33e7c5b76b1cae3902e0f98090e1a164e425e
Gustav Mörtberg <gustavmortberg@gmail.com>
2015-05-20 13:20:35 +0200

    Updated utils.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,8 @@
 module utils {
+    /**
+     * "Pauses" execution for approximately the specified ms, used for debugging purposes.
+     * @param milliseconds Milliseconds to pause execution for.
+     */
     export function sleep(milliseconds) {
         var start = new Date().getTime();
         for (var i = 0; i < 1e7; i++) {
@@ -8,6 +12,11 @@ module utils {
         }
     }
 
+    /**
+     * Returns a random integer between 0 and max.
+     * @param max           Maximum of returned integer.
+     * @returns {number}    Integer between 0 and max.
+     */
     function getRandomInt(max) {
         return Math.floor(Math.random() * max);
 
