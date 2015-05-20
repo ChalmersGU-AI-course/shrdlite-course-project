3c6def1c091b7f6697dded49aa12ed053150d980
Ronakorn Soponpunth <peach.ronakorn@gmail.com>
2015-05-20 13:19:55 +0200

    updated interpreter

--- Interpreter.ts
+++ Interpreter.ts
@@ -1,6 +1,6 @@
 ///<reference path="World.ts"/>
 ///<reference path="Parser.ts"/>
-///<reference path="Astar/collections.ts"/>
+///<reference path="AstarPlanner/collections.ts"/>
 
 
 module Interpreter {
@@ -59,7 +59,7 @@ module Interpreter {
 
     function interpretCommand(cmd : Parser.Command, state : WorldState) : Literal[][] {
 
-        console.log(cmd);
+        // console.log(cmd);
         console.log(state);
         var intprt : Literal[][] = [];
         var tokens : string[] = [];
@@ -99,6 +99,7 @@ module Interpreter {
 
         //case move||put||drop entity location === move ent loc
         //parse the result from cmd into new finite Array named tokens
+        //separated by x to support ambiguous 
         if(cmd.cmd == "move"){
             var currentEnt = cmd.ent;
             var currentLoc = cmd.loc;
@@ -109,6 +110,7 @@ module Interpreter {
             if(currentEnt.obj != null){
                 var objArrays = recursiveObject(currentEnt.obj);
                 tokens = tokens.concat(objArrays);
+                tokens.push("x");
             }
 
             if(currentLoc != null){
@@ -139,150 +141,346 @@ module Interpreter {
        
         var rules : Literal[] = [];
 
-        var selPol = true;
-        var selRel = "";
-        var foundRel = false;
         var foundForm = false;
-        var maxArg = 0;
-        var currentArg = 0;
         var selSize = "";
         var selColor = "";
         var selForm = "";
-        var prevObj = "";
 
         //case take cmd
         if(tokens[0] == "take"){
-            selPol = true;
-            selRel = "holding";
-            foundRel = true;
-            maxArg = 1;
-            currentArg = 0;
+            var rels : string[] = [];
+            var objs : string[][] = [];
+
+            for(var i =1;i< tokens.length ;i++){
+
+                var token = tokens[i];
+                var tokenType = globalDic.getValue(token);
+
+                switch(tokenType) {
+
+                    case "size":
+                        selSize = token;
+                        break;
+                    case "color":
+                        selColor = token;
+                        break;
+                    case "form":
+                        selForm = token;
+                        foundForm = true;
+                        break;
+                    case "rel":
+                        rels.push(token)
+                        break;
+                    case "quant": 
+                        //... to do
+                        break;
+                }
+
+                if(foundForm){
+                    var founds = searchObject(selSize,selColor,selForm,state);
+                    objs.push(founds);
+                    foundForm = false;
+                    selSize = "";
+                    selColor = "";
+                    selForm = "";
+                }
+            }
+
+            var goalObj = findObject(objs,rels,state);
+            if(goalObj == "")
+                return null;
+            else 
+                return [{pol:true, rel:"holding", args:[goalObj]}];
+
         }
         //case put cmd
         if(tokens[0] == "put"){
-            selPol = false;
-            selRel = "holding";
-            foundRel = true;
-            foundForm = true;
-            maxArg = 2;
-            currentArg = 1;
+            var rels : string[] = [];
+            var objs : string[][] = [];
+            var sRel = tokens[1];
+            var splitIndex = 1;
+            var foundX = false;
+            var objA = state.holding;
+            var objB = "";
 
-            //hard code for testing
-            //the object "e" should be the value from state.holding
-            var newRule : Literal = {pol: selPol, rel: selRel, args: ["e"]};
-            rules.push(newRule);
-            prevObj = "e";
+            for(var i = 2;i< tokens.length ;i++){
+
+                var token = tokens[i];
+                var tokenType = globalDic.getValue(token);
+
+                switch(tokenType) {
+
+                    case "size":
+                        selSize = token;
+                        break;
+                    case "color":
+                        selColor = token;
+                        break;
+                    case "form":
+                        selForm = token;
+                        foundForm = true;
+                        break;
+                    case "rel":
+                        rels.push(token)
+                        break;
+                    case "quant": 
+                        //... to do
+                        break;
+                }
+
+                if(foundForm){
+                    var founds = searchObject(selSize,selColor,selForm,state);
+                    objs.push(founds);
+                    foundForm = false;
+                    selSize = "";
+                    selColor = "";
+                    selForm = "";
+                }
+            }
+
+            objB = findObject(objs,rels,state);
+            if(objB == "z")
+                objB = "floor";
+
+            if(objA == "" || objB == ""){
+                return null;
+            }
+            else
+                return [{pol:true, rel:sRel, args:[objA,objB]}];
 
         }
+
         //case move cmd
         if(tokens[0] == "move"){
-            selPol = true;
-            selRel = "";
-            foundRel = false;
-            maxArg = 2;
-            currentArg = 0;
-        }
+            var rels : string[] = [];
+            var objs : string[][] = [];
+            var sRel = "";
+            var splitIndex = 1;
+            var foundX = false;
+            var objA = "";
+            var objB = "";
 
-        for(var i = 1;i < tokens.length; i++){
+            for(var i =1;i< tokens.length ;i++){
 
-            var token = tokens[i];
-            var tokenType = globalDic.getValue(token);
+                var token = tokens[i];
+                var tokenType = globalDic.getValue(token);
 
-            switch(tokenType) {
+                switch(tokenType) {
 
-                case "size":
-                    selSize = token;
-                    break;
-                case "color":
-                    selColor = token;
-                    break;
-                case "form":
-                    selForm = token;
-                    foundForm = true;
-                    currentArg++;
-                    break;
-                case "rel":
-                    selRel = token;
-                    foundRel = true;
-                    maxArg = 2;
-                    break;
-                case "quant": 
-                    //... to do
-                    break;
+                    case "size":
+                        selSize = token;
+                        break;
+                    case "color":
+                        selColor = token;
+                        break;
+                    case "form":
+                        selForm = token;
+                        foundForm = true;
+                        break;
+                    case "rel":
+                        if(foundX){
+                            foundX = false;
+                            sRel = token;
+                        }else{
+                            rels.push(token)
+                        }
+                        break;
+                    case "quant": 
+                        //... to do
+                        break;
 
-                default:
-                    console.log("token error...");
-
-            }
-            if(currentArg != maxArg && !foundRel && foundForm){
-                var currentObj = searchObject(selSize,selColor,selForm,state);
-                if(currentObj == ""){
-                    return null;
+                    default:
+                        //case "x" for ambiguous
+                        splitIndex = i;
+                        foundX = true;
                 }
 
-                foundForm = false;
-                selSize = "";
-                selColor = "";
-                selForm = "";
-                prevObj = currentObj;
-
-            }
-
-            if(currentArg == maxArg && foundRel && foundForm){
-                var currentObj = searchObject(selSize,selColor,selForm,state);
-
-                if(currentObj == ""){
-                    return null;
-                }
-
-                if(maxArg == 1){
-                    var newRule : Literal = {pol: selPol, rel: selRel, args: [currentObj]};
-                    rules.push(newRule);
-
-                    selPol = true;
-                    selRel = "";
-                    foundRel = false;
+                if(foundForm){
+                    var founds = searchObject(selSize,selColor,selForm,state);
+                    objs.push(founds);
                     foundForm = false;
-                    maxArg = 2;
-                    currentArg = 1;
                     selSize = "";
                     selColor = "";
                     selForm = "";
-                    prevObj = currentObj;
+                }
 
-                }else if(maxArg == 2){
-                    var newRule : Literal = {pol: true, rel: selRel, args: [prevObj,currentObj]};
-                    rules.push(newRule);
-
-                    selPol = true;
-                    selRel = "";
-                    foundRel = false;
-                    foundForm = false;
-                    maxArg = 2;
-                    currentArg = 1;
-                    selSize = "";
-                    selColor = "";
-                    selForm = "";
-                    prevObj = currentObj;
-
+                if(foundX){
+                    objA = findObject(objs,rels,state);
+                    rels = [];
+                    objs = [];
                 }
             }
+
+            objB = findObject(objs,rels,state);
+            if(objB == "z")
+                objB = "floor";
+
+            if(objA == "" || objB == ""){
+                return null;
+            }
+            else
+                return [{pol:true, rel:sRel, args:[objA,objB]}];
+
+
         }
 
-
-        return rules;
     }
+
+    function findObject(objs : string[][], rels : string[], state : WorldState) : string {
+        console.log(objs);
+
+        //case simple object, no relation
+        if(rels.length == 0){
+            //no object found
+            if(objs.length == 0){
+                return "";
+            }
+            else{
+                return objs[0][0];
+            }
+        }
+        else{
+            var combinations = allCombinations(objs);
+            console.log(combinations);
+
+            for(var i = 0 ;i < combinations.length; i++){
+
+                var combArray = combinations[i].split("");
+
+                if(checkValidObject(combArray,rels,state)){
+                    return combArray[0];
+                }
+            }
+
+        }
+
+        return "";
+
+    }
+
+    function checkValidObject(objs : string[], rels : string[], state : WorldState) : boolean {
+        var numberOfLoop = rels.length;
+        for(var i = 0;i< numberOfLoop; i++){
+            if(!checkPredicate(objs[i],objs[i+1],rels[i],state)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    interface stackPosition {x:number; y:number;};
+
+    // Function for finding the x and y position of an object in the stacks (x = column, y = row)
+    function getStackLocation(object : string , state : WorldState) : stackPosition {
+        for(var x = 0; x < state.stacks.length ; x++){
+            for(var y = 0 ; y < state.stacks[x].length ; y++){
+                if(state.stacks[x][y] == object){
+                    return {x:x, y:y};
+                }
+            }
+        }
+    }
+
+    function checkPredicate(obj1 : string, obj2 : string, rel : string, state : WorldState) : boolean {
+        var a_loc = getStackLocation(obj1,state);
+        var b_loc = getStackLocation(obj2,state);
+
+        //special case for "floor"
+        if(obj2 == "z"){
+            if(a_loc.y == 0)
+                return true;
+            else
+                return false;
+        }
+        if(obj1 == "z")
+            return null;
+
+
+        var result = false;
+
+        switch(rel){
+            case "inside":
+                //it should somehow check that obj1 should be in form of Box otherwise return error
+                result = (a_loc.x == b_loc.x) && (a_loc.y == (b_loc.y + 1));
+                break;
+            case "ontop":
+                result = (a_loc.x == b_loc.x) && (a_loc.y == (b_loc.y + 1));
+                break;
+            case "above":
+                result = (a_loc.x == b_loc.x) && (a_loc.y > b_loc.y);
+                break;
+            case "under":
+                result = (a_loc.x == b_loc.x) && (a_loc.y < b_loc.y);
+                break;
+            case "beside":
+                result = ((a_loc.x == (b_loc.x +1)) || (a_loc.x == (b_loc.x -1)))  && (a_loc.y == b_loc.y);
+                break;
+            case "leftof":
+                result = (a_loc.x == (b_loc.x -1)) && (a_loc.y == b_loc.y);
+                break;
+            case "rightof":
+                result = (a_loc.x == (b_loc.x +1)) && (a_loc.y == b_loc.y);
+                break;
+        }
+
+        console.log(result);
+        return result;
+    }
+
+
+    function checkLaws(obj1 : string, obj2 : string, rel : string, state : WorldState) : boolean {
+
+        var detail1 = state.objects[obj1];
+        var detail2 = state.objects[obj2];
+
+        var result = false;
+
+        switch(rel){
+            case "inside":
+                if(detail2.form == "box"){
+                    if(detail2.size == "small"){
+                        result = (detail1.size == "small") && (detail1.form == "brick" || detail1.form == "ball" || detail1.form == "table");
+                    }
+                    if(detail2.size == "large"){
+                        result = detail1.size == "small" || detail1.form == "brick" || detail1.form == "ball" || detail1.form == "table";
+                    }
+                }
+                break;
+            default:
+            result = true;   
+
+        }
+
+
+        return result;
+    }
+
+    function allCombinations(arr) {
+        if (arr.length == 1) {
+            return arr[0];
+        } else {
+            var result = [];
+            var allCasesOfRest = allCombinations(arr.slice(1));  // recur with the rest of array
+            for (var i = 0; i < allCasesOfRest.length; i++) {
+              for (var j = 0; j < arr[0].length; j++) {
+                result.push(arr[0][j] + allCasesOfRest[i]);
+              }
+            }
+            return result;
+        }
+    }
+
 
     //search object representation from size, color, form
     //return an object representation in current world e.g. "a", "b", "c"
     //return emptystring if there is no object found in current world so it means that impossible to be solved
-    function searchObject(size : string, color : string, form : string, state : WorldState) : string {
+    function searchObject(size : string, color : string, form : string, state : WorldState) : string[] {
 
-        //it can match more than 1 object that can cause ambiguous
-        //but skipped ambiguous by now so it always match the first item in current world
-        
+        var results : string[] = [];
+
         if(form == "floor")
-            return "floor";
+            return ["z"];
 
         var objs : string[] = Array.prototype.concat.apply([], state.stacks);
         var matchCount = 0;
@@ -314,10 +512,11 @@ module Interpreter {
             }
 
             if(matchCount == maxMatchCount){
-                return objs[i];
+                results.push(objs[i]);
             }
         }
-       return ""; 
+       // console.log(results);
+       return results; 
 
     }
 
