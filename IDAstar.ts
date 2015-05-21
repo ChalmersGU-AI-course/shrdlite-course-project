930e3eced8a1cc446ee70e75c969eb8b75e0e2a9
Leif Schelin <schelinleif@gmail.com>
2015-05-21 08:31:02 +0200

    This just might work...

--- IDAstar.ts
+++ IDAstar.ts
@@ -1,15 +1,38 @@
 
-
 /// <reference path="lib/collections.ts" />
-/// <reference path="Astar.ts"/>
 
 module IDAstar{
     //-- Interfaces -------------------------------------------
 
     export class Error implements Error {
-        public name = "IDAstar.Error";
+        public name = "IDError";
         constructor(public message? : string) {}
         public toString() {return this.name + ": " + this.message}
+    }
+
+    export interface Neighbours<T>{
+        (state : T) : Neighb<T>[] ;
+    }
+
+    export interface Neighb<T>{
+        state : T ;
+        action : string ;
+        transitionCost : number ;
+    }
+
+    /**
+    * Heuristic function.
+    */
+    export interface Heuristic<T>{
+        (state : T) : number ;
+    }
+
+    /**
+    * returns true iff the state is accepting, ie is a goal state.
+    * Thus, there can be several goal states.
+    */
+    export interface Goal<T>{
+        (state : T) : boolean ;
     }
 
     /**
@@ -18,18 +41,96 @@ module IDAstar{
     interface Vertex<T>{
         state : T ;
         cost : number ;
+        heur : number ;
         previous : number ;
         action : string ;
+    }
+
+    // Depth first search until a heuristic limit.
+    // Returns if the goal was found.
+    // Goal vertex can be found in `s.order[s.x]`
+    function depthFirstUntil<T>(s : Search<T>) : boolean{
+        var stack = new collections.Stack<Vertex<T>>();
+
+        // TODO improvement: heuristic depth-first instead of simple depth first?
+        while(! stack.isEmpty){
+            var v : Vertex<T> = stack.pop();
+            s.order[s.x] = v;
+
+            if(s.isGoal(v.state)){
+                return true;
+            }
+
+            var neighbours : Neighb<T>[] = s.f(v.state);
+            for(var n in neighbours){
+                var neighb = neighbours[n];
+                var vn : Vertex<T> = {
+                    state : neighb.state,
+                    cost : v.cost + neighb.transitionCost,
+                    heur : s.h(neighb.state),
+                    previous : s.x,
+                    action : neighb.action
+                };
+
+                if(vn.heur < s.hLimit){
+                    stack.push(vn);
+                } else {
+                    s.prioQueue.enqueue(vn);
+                }
+
+            }
+
+            s.x = s.x + 1;
+        }
+
+        return false;
+    }
+
+    function idaSearch<T>(s : Search<T>) : string[] {
+        while(! depthFirstUntil<T>(s)){
+            s.hLimit = 2*s.hLimit ;
+        }
+        return postProcess(s.order, s.x);
+    }
+
+    export class Search<T>{
+        public prioQueue : collections.PriorityQueue<Vertex<T>> ;
+        public x : number = 0;
+        public order : Array<Vertex<T>> = [];
+        public startVertex : Vertex<T>
+
+        constructor(
+            start : T,
+            public hLimit : number,
+            public f : Neighbours<T>,
+            public h : Heuristic<T>,
+            public isGoal : Goal<T>,
+            public maxIter : number = 25000,
+            public multiPathPruning : boolean = true
+        ){
+            this.prioQueue = new collections.PriorityQueue<Vertex<T>>(( (a, b) => {
+                return b.cost + b.heur - a.cost - a.heur;
+            } )) ;
+
+            this.startVertex = {
+                state : start,
+                cost : 0,
+                heur : h(start),
+                previous : -1,
+                action : "init"
+            };
+        }
     }
 
     /**
     * Type alias used for comparison in the PriorityQueue.
     */
-    interface Compare<T> {
+
+    /*interface Compare<T> {
         icf : collections.ICompareFunction<Vertex<T>>;
     }
 
-    export function OLDastar<T>(f : Astar.Neighbours<T>, c : Astar.Cost<T>, h : Astar.Heuristic<T>, start : T, isGoal : Astar.Goal<T>, multiPathPruning : boolean = true, maxIter : number = 25000 ) : string[]{
+    export function OLDastar<T>(f : Neighbours<T>, c : Cost<T>, h : Heuristic<T>, start : T, isGoal : Goal<T>, multiPathPruning : boolean = true, maxIter : number = 25000 ) : string[]{
         var comp : Compare<T> = {
             icf : ((a, b) => {
                 return b.cost + h(b.state) - a.cost - h(a.state);
@@ -38,7 +139,7 @@ module IDAstar{
         return OLDsearch<T>(comp, f, c, h, start, isGoal, multiPathPruning, maxIter);
     }
 
-    function OLDsearch<T>(comp : Compare<T>, f : Astar.Neighbours<T>, c : Astar.Cost<T>, h : Astar.Heuristic<T>, start : T, isGoal : Astar.Goal<T>, multiPathPruning : boolean = true, maxIter : number = 25000 ) : string[]{
+    function OLDsearch<T>(comp : Compare<T>, f : Neighbours<T>, c : Cost<T>, h : Heuristic<T>, start : T, isGoal : Goal<T>, multiPathPruning : boolean = true, maxIter : number = 25000 ) : string[]{
         var queue = new collections.PriorityQueue<Vertex<T>>(comp.icf) ;
 
         var order : Array<Vertex<T>> = [];
@@ -54,7 +155,7 @@ module IDAstar{
         for( var x = 0; ! queue.isEmpty(); ++x){
 
             if(x > maxIter){
-                throw new IDAstar.Error("Stopping early after " + x + " iterations. Size of queue: " + queue.size() + " current cost: " + current.cost);
+                throw new IDError("Stopping early after " + x + " iterations. Size of queue: " + queue.size() + " current cost: " + current.cost);
                 return postProcess<T>(order, x-1);
                 // return showVisited<T>(order);
             }
@@ -93,10 +194,11 @@ module IDAstar{
         }
 
         // console.log("No solution found!");
-        throw new IDAstar.Error("No solution found!");
+        throw new IDError("No solution found!");
 
         return ["init"];
     }
+    */
 
     /**
     * Backtracks from the final state to the original state.
