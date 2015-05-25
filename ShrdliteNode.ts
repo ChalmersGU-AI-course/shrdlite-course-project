245f8dd35b317d7e5b4685f37fa39853f273a2e2
Markus Lindelöw <markus@lindeloew.se>
2015-05-25 16:32:41 +0200

    Refactored some file structure

---
+++ ShrdliteNode.ts
@@ -0,0 +1,131 @@
+﻿///<reference path="Graph.ts"/>
+///<reference path="World.ts"/>
+
+class ShrdliteNode implements GraphNode<number> {
+    public name: string;
+    public updateName() {
+        this.name = this.toString();
+    }
+    private objects: { [s: string]: ObjectDefinition; };
+
+    public constructor(public state: WorldState) {
+        this.name = this.toString();
+    }
+
+
+    costTo(to: ShrdliteNode): number {
+        return (Math.abs(to.state.arm - this.state.arm) + 1);
+    }
+
+    neighbours(): { node: ShrdliteNode; edge: number }[] {
+        var path: Array<{ node: ShrdliteNode; edge: number }> = [];
+
+        //take up object
+        if (this.state.holding == null) {
+
+            for (var i = 0; i < this.state.stacks.length; ++i)
+                if (this.state.stacks[i].length > 0) {
+                    var newNode = new ShrdliteNode(this.cloneOfWorld());
+                    newNode.state.arm = i;
+                    newNode.state.holding = newNode.state.stacks[i].pop();
+                    newNode.updateName(); //Overhead but nice anyway
+                    path.push({ node: newNode, edge: i });
+                }
+        }
+        else {
+                
+            //put down object
+            for (var i = 0; i < this.state.stacks.length; ++i) {
+                if (this.isPhysicallyPossible(i)) {
+                    var newNode = new ShrdliteNode(this.cloneOfWorld());
+                    newNode.state.arm = i;
+                    newNode.state.stacks[i].push(newNode.state.holding);
+                    newNode.state.holding = null;
+                    newNode.updateName(); //Overhead but nice anyway
+                    path.push({ node: newNode, edge: i });
+                }
+            }
+        }
+        return path;
+    }
+
+//Some strange encodingerror with the comment
+    /**
+	* Check if the transition is physically possible
+	*
+	* @param {number} number of the stack you want to check
+	* @return {boolean} true if it is possible
+	*/
+    isPhysicallyPossible(n: number): boolean {
+        //which object is on top?
+        var topPos: number = this.state.stacks[n].length - 1;
+
+        //destination object
+        var dest;
+        if (topPos >= 0) { //there is actually an object on this stack
+            dest = this.state.objects[this.state.stacks[n][topPos]];
+        } else { //otherwise use the floor object
+            dest = this.state.objects["floor"];
+        }
+
+        //origin object from the crane
+        var orig = this.state.objects[this.state.holding];
+
+        //Balls must be in boxes or on the floor.
+        if (orig.form == "ball" && dest.form != "floor" && dest.form != "box") {
+            return false;
+        }
+
+        //Large boxes cannot be supported by large pyramids.
+        if ((orig.size == "large" && orig.form == "box") && (dest.size == "large" && dest.size == "pyramid")) {
+            return false;
+        }
+
+        //Small boxes cannot be supported by small bricks or pyramids.
+        if ((orig.size == "small" && orig.form == "box") && (dest.size == "small" && (dest.form == "pyramid" || dest.form == "brick"))) {
+            return false;
+        }
+
+        //Boxes cannot contain pyramids, planks or boxes of the same size.
+        if ((orig.form == "pyramid" || orig.form == "box" || orig.form == "plank") && (orig.size == dest.size) && (dest.form == "box")) {
+            return false;
+        }
+
+        //Balls cannot support other objects.
+        if (dest.form == "ball") {
+            return false;
+        }
+
+        //Small objects cannot support large objects.
+        if (orig.size == "large" && dest.size == "small") {
+            return false;
+        }
+
+        return true;
+    }
+
+    toString(): string {
+        var str: string = "S:";
+        for (var i = 0; i < this.state.stacks.length; ++i) {
+            for (var j = 0; j < this.state.stacks[i].length; ++j) {
+                str += this.state.stacks[i][j] + ",";
+            }
+            str += ";"
+        }
+        str += ";H:" + this.state.holding + ";";
+
+        return str;
+    }
+
+    private cloneOfWorld(): WorldState {
+        var stackcopy: string[][] = [];
+
+        for (var i = 0; i < this.state.stacks.length; ++i) {
+            stackcopy[i] = [];
+            for (var j = 0; j < this.state.stacks[i].length; ++j)
+                stackcopy[i][j] = this.state.stacks[i][j];
+        }
+
+        return {arm: this.state.arm, examples: this.state.examples, holding: this.state.holding, objects: this.state.objects, stacks: stackcopy };
+    }
+}
---
+++ ShrdliteNodeFilter.ts
@@ -0,0 +1,74 @@
+﻿///<reference path="Graph.ts"/>
+///<reference path="World.ts"/>
+
+class ShrdliteNodeFilter implements GraphFilter {
+    public constructor(protected intptr: Interpreter.Literal) {
+
+    }
+    public costTo(node: ShrdliteNode): number {
+        switch (this.intptr.rel) {
+            case 'holding':
+                if (node.state.holding == this.intptr.args[0])
+                    return 0;
+                break;
+            case 'ontop':
+            case 'inside':
+                for (var i = 0; i < node.state.stacks.length; ++i) {
+                    var below = node.state.stacks[i].indexOf(this.intptr.args[1]);
+                    var onTop = node.state.stacks[i].indexOf(this.intptr.args[0]);
+
+                    if ((below != -1 || this.intptr.args[1] == 'floor') && onTop != -1 && below + 1 == onTop)
+                        return 0;
+                }
+                break;
+            case 'above':
+                for (var i = 0; i < node.state.stacks.length; ++i) {
+                    var below = node.state.stacks[i].indexOf(this.intptr.args[1]);
+                    var onTop = node.state.stacks[i].indexOf(this.intptr.args[1]);
+
+                    if ((below != -1 || this.intptr.args[1] == 'floor') && onTop != -1 && below < onTop)
+                        return 0;
+                }
+                break;
+            case 'beside':
+                for (var i = 0; i < node.state.stacks.length; ++i) {
+                    var below = node.state.stacks[i].indexOf(this.intptr.args[1]);
+                    var onTop = node.state.stacks[i].indexOf(this.intptr.args[0]);
+
+                    //leftof or rightoff
+
+                    if ((below != -1 || this.intptr.args[1] == 'floor') && onTop != -1 && below + 1 == onTop)
+                        return 0;
+                }
+                break;
+            case 'leftof':
+                for (var i = 1; i < node.state.stacks.length; ++i) {
+                    var leftObject: boolean = node.state.stacks[i - 1].indexOf(this.intptr.args[0]) != -1;
+                    var rightObject: boolean = node.state.stacks[i].indexOf(this.intptr.args[1]) != -1;
+                    if (leftObject && rightObject)
+                        return 0;
+                }
+                break;
+            case 'rightof':
+                for (var i = 1; i < node.state.stacks.length; ++i) {
+                    var leftObject: boolean = node.state.stacks[i - 1].indexOf(this.intptr.args[1]) != -1;
+                    var rightObject: boolean = node.state.stacks[i].indexOf(this.intptr.args[0]) != -1;
+                    if (leftObject && rightObject)
+                        return 0;
+                }
+                break;
+            case 'under':
+                for (var i = 0; i < node.state.stacks.length; ++i) {
+                    var below = node.state.stacks[i].indexOf(this.intptr.args[1]);
+                    var onTop = node.state.stacks[i].indexOf(this.intptr.args[1]);
+
+                    if ((below != -1 || this.intptr.args[1] == 'floor') && onTop != -1 && below < onTop)
+                        return 0;
+                }
+                break;
+            default:
+                break;
+        }
+        return 1;
+    }
+}
