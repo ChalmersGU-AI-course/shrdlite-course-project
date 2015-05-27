17f8c65c8115c2bd4daca7774fe11b63558d2ddf
Loulou54 <louis.durand@telecom-bretagne.eu>
2015-05-27 12:40:59 +0200

    Changed Interpreter to consider each possible parse
    If several parses, interprets them all and takes the one with lower
    heuristic.

--- src/Interpreter.ts
+++ src/Interpreter.ts
@@ -18,11 +18,17 @@ module Interpreter {
         parses.forEach((andProp) => {
             inter.intp.push([]);
             andProp.forEach((prop) => {
-                var lits : Literal[] = chooseBest(interpretCommand(prop[0].prs, currentState), currentState.stacks);
-                if(!lits) {
+                var litPars : Literal[][] = [];
+                prop.forEach((res) => {
+                    var lits : Literal[] = chooseBest(interpretCommand(res.prs, currentState), currentState.stacks);
+                    if(lits) {
+                        litPars.push(lits);
+                    }
+                });
+                if(litPars.length==0) {
                     throw new Interpreter.Error("Found no interpretation");
                 }
-                lits.forEach((l) => {inter.intp[inter.intp.length-1].push(l)});
+                chooseBest(litPars, currentState.stacks).forEach((l) => {inter.intp[inter.intp.length-1].push(l)});
             });
 
         });
