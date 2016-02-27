737791493a4217b88d2b6693f624dcab0dd86ab5
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2016-02-27 09:44:35 +0100

    Template for graphs and astar search

---
+++ Graph.ts
@@ -0,0 +1,134 @@
+///<reference path="lib/collections.ts"/>
+
+class Edge<Node> {
+    from : Node;
+    to   : Node;
+    cost : number;
+}
+
+interface Graph<Node> {
+    outgoingEdges(node : Node) : Edge<Node>[];
+    compareNodes : collections.ICompareFunction<Node>;
+}
+
+class SearchResult<Node> {
+    path : Node[];
+    cost : number;
+}
+
+
+function aStarSearch<Node> (
+    graph : Graph<Node>,
+    start : Node,
+    goal : (Node) => boolean,
+    heuristics : (Node) => number,
+    timeout : number
+) : SearchResult<Node> {
+    // A dummy search result: it just picks the first possible neighbour
+    var result : SearchResult<Node> = {
+        path: [start],
+        cost: 0
+    };
+    while (result.path.length < 3) {
+        var edge : Edge = graph.outgoingEdges(start) [0];
+        if (! edge) break;
+        start = edge.to;
+        result.path.push(start);
+        result.cost += edge.cost;
+    }
+    return result;
+}
+
+
+//////////////////////////////////////////////////////////////////////
+// here is an example graph
+
+interface Coordinate {
+    x : number;
+    y : number;
+}
+
+
+class GridNode {
+    constructor(
+        public pos : Coordinate
+    ) {}
+
+    add(delta : Coordinate) : GridNode {
+        return new GridNode({
+            x: this.pos.x + delta.x,
+            y: this.pos.y + delta.y
+        });
+    }
+
+    compareTo(other : GridNode) : number {
+        return (this.pos.x - other.pos.x) || (this.pos.y - other.pos.y);
+    }
+
+    toString() : string {
+        return "(" + this.pos.x + "," + this.pos.y + ")";
+    }
+}
+
+
+class GridGraph implements Graph<GridNode> {
+    private walls : collections.Set<GridNode>;
+
+    constructor(
+        public size : Coordinate,
+        obstacles : Coordinate[]
+    ) {
+        this.walls = new collections.Set<GridNode>();
+        for (var pos of obstacles) {
+            this.walls.add(new GridNode(pos));
+        }
+        for (var x = -1; x <= size.x; x++) {
+            this.walls.add(new GridNode({x:x, y:-1}));
+            this.walls.add(new GridNode({x:x, y:size.y}));
+        }
+        for (var y = -1; y <= size.y; y++) {
+            this.walls.add(new GridNode({x:-1, y:y}));
+            this.walls.add(new GridNode({x:size.x, y:y}));
+        }
+    }
+
+    outgoingEdges(node : GridNode) : Edge<GridNode>[] {
+        var outgoing : Edge<GridNode>[] = [];
+        for (var dx = -1; dx <= 1; dx++) {
+            for (var dy = -1; dy <= 1; dy++) {
+                if (! (dx == 0 && dy == 0)) {
+                    var next = node.add({x:dx, y:dy});
+                    if (! this.walls.contains(next)) {
+                        outgoing.push({
+                            from: node,
+                            to: next,
+                            cost: Math.sqrt(dx*dx + dy*dy)
+                        });
+                    }
+                }
+            }
+        }
+        return outgoing;
+    }
+
+    compareNodes(a : GridNode, b : GridNode) : number {
+        return a.compareTo(b);
+    }
+
+    toString() : string {
+        var borderRow = "+" + new Array(this.size.x + 1).join("--+");
+        var betweenRow = "+" + new Array(this.size.x + 1).join("  +");
+        var str = "\n" + borderRow + "\n";
+        for (var y = this.size.y-1; y >= 0; y--) {
+            str += "|";
+            for (var x = 0; x < this.size.x; x++) {
+                str += this.walls.contains(new GridNode({x:x,y:y})) ? "## " : "   ";
+            }
+            str += "|\n";
+            if (y > 0) str += betweenRow + "\n";
+        }
+        str += borderRow + "\n";
+        return str;
+    }
+}
+
