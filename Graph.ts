a818760107dfd6832e5b572de890a339eecf7ea4
sirmackan <marcus@meeloo.se>
2016-04-26 15:30:15 +0200

    Implemented first version of A*

--- Graph.ts
+++ Graph.ts
@@ -55,18 +55,74 @@ function aStarSearch<Node> (
     heuristics : (n:Node) => number,
     timeout : number
 ) : SearchResult<Node> {
-    // A dummy search result: it just picks the first possible neighbour
-    var result : SearchResult<Node> = {
-        path: [start],
-        cost: 0
-    };
-    while (result.path.length < 3) {
-        var edge : Edge<Node> = graph.outgoingEdges(start) [0];
-        if (! edge) break;
-        start = edge.to;
-        result.path.push(start);
-        result.cost += edge.cost;
+    var openSet = new collections.Set<Node>();
+    var closedSet = new collections.Set<Node>();
+
+    openSet.add(start);
+
+    var gScore = new collections.Dictionary<Node, number>();
+    gScore.setValue(start, 0);
+
+    var fScore = new collections.Dictionary<Node, number>();
+    fScore.setValue(start, heuristics(start));
+
+    var parent = new collections.Dictionary<Node, Node>();
+
+    while (!openSet.isEmpty()) {
+        var minFScore = Infinity;
+        var current : Node;
+
+        openSet.forEach(function(node) {
+            if (fScore.getValue(node) < minFScore) {
+                minFScore = fScore.getValue(node);
+                current = node;
+            }
+        });
+
+        if (goal(current)) {
+            var result : SearchResult<Node> = {
+                path: [current],
+                cost: gScore.getValue(current)
+            };
+
+            while (parent.containsKey(current)) {
+                current = parent.getValue(current);
+                result.path.push(current);
+            }
+
+            result.path.reverse();
+            return result;
+        }
+
+        openSet.remove(current);
+        closedSet.add(current);
+
+        for (var edge of graph.outgoingEdges(current)) {
+            var neighbor = edge.to;
+
+            if (closedSet.contains(neighbor)) {
+                continue;
+            }
+
+            var _gScore = gScore.getValue(current) + edge.cost;
+
+            if (!openSet.contains(neighbor)) {
+                openSet.add(neighbor);
+            } else if (_gScore >= gScore.getValue(neighbor)) {
+                continue;
+            }
+
+            parent.setValue(neighbor, current);
+            gScore.setValue(neighbor, _gScore);
+            fScore.setValue(neighbor, _gScore + heuristics(neighbor));
+        }
     }
+
+    var result : SearchResult<Node> = {
+        path: [],
+        cost: Infinity
+    };
+
     return result;
 }
 
