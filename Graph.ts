4c7894f2e61015b0137a5e3ea7888b8a67600a48
Tobias Ekblom <tobias.ekblom@applandinc.com>
2016-04-29 17:06:27 +0200

    Nearly finished, missing comparison of node objects

--- Graph.ts
+++ Graph.ts
@@ -60,14 +60,129 @@ function aStarSearch<Node> (
         path: [start],
         cost: 0
     };
-    while (result.path.length < 3) {
-        var edge : Edge<Node> = graph.outgoingEdges(start) [0];
-        if (! edge) break;
-        start = edge.to;
-        result.path.push(start);
-        result.cost += edge.cost;
+
+    var start_nwp: NodeWithPath<Node> = new NodeWithPath(start);
+
+    // The nodes already evaluated
+    var closedSet:NodeWithPath<Node>[] = [];
+
+    // Discovered node still not evaluated
+    var openSet:NodeWithPath<Node>[] = [];
+    openSet.push(start_nwp);
+
+    var cameFrom:NodeWithPath<Node>[] = [];
+
+    start_nwp.gScore = 0;
+    //console.log(heuristics);
+    start_nwp.fScore = heuristics(start_nwp.my_node);
+    //console.log(nwp.fScore);
+    var x = 0;
+    while (openSet.length > 0 && x < 4) {
+      //openSet
+      var current = getOpenSetLowestfScore(openSet);
+      if (goal(current.my_node)) {
+        console.log("We found shortest way!");
+        break;
+      }
+      // remove nodewitpath from openSet
+      var index = openSet.indexOf(current, 0);
+      if (index > -1) {
+        openSet.splice(index, 1);
+      }
+      closedSet.push(current);
+      console.log(current.my_node);
+      console.log(closedSet.length);
+
+      var neigboors: Edge<Node>[] = graph.outgoingEdges(current.my_node);
+      for (let neighbor_edge of neigboors) {
+        var neighboor_nwp: NodeWithPath<Node> = new NodeWithPath(neighbor_edge.to);
+        console.log("Looking in closed list");
+        if (isNodeInList(graph, closedSet, neighbor_edge.to)) {
+          console.log("Node was already visited");
+          continue;
+        }
+        var tentative_gScore:number = current.gScore + neighbor_edge.cost; // only one step to neighbor
+        console.log("Looking in open list");
+        if (!isNodeInList(graph, openSet, neighbor_edge.to)) {
+          openSet.push(new NodeWithPath(neighbor_edge.to));
+        } else if (tentative_gScore >= getGScoreForNode(openSet, neighbor_edge.to)) {
+          continue;
+        }
+        neighboor_nwp.my_parent_node = current;
+        neighboor_nwp.gScore = tentative_gScore;
+        neighboor_nwp.fScore = tentative_gScore + heuristics(neighboor_nwp.my_node);
+      }
+      console.log("sa");
+      x++;
     }
+
     return result;
 }
 
 
+class NodeWithPath<Node> {
+  //public my_node: Node;
+  public my_parent_node: NodeWithPath<Node>;
+  public gScore: number = 10000000;
+  public fScore: number = 10000000;
+
+  constructor(public my_node : Node) {
+
+  }
+}
+function getGScoreForNode<Node>(list: NodeWithPath<Node>[], element: Node) {
+  for(let nwp of list) {
+    if(nwp.my_node  == element) {
+        return nwp.gScore;
+    }
+  }
+  return -1;
+}
+
+function isNodeInList<Node>(graph: Graph<Node>, list: NodeWithPath<Node>[], element: Node) {
+  for(let nwp of list) {
+    //if (nwp.my_node.pos.x  == element.pos.x &&
+    //nwp.my_node.pos.y  == element.pos.y) {
+    if (nwp.my_node == element) {
+      console.log("Was in list");
+      return true;
+    }
+  }
+  console.log("Was not in list");
+  return false;
+}
+
+function getOpenSetLowestfScore<Node>(openSet: NodeWithPath<Node>[]): NodeWithPath<Node> {
+  var lowest_nwp: NodeWithPath<Node> = null;
+  for(let nwp of openSet) {
+    if (lowest_nwp == null) {
+      lowest_nwp = nwp;
+      continue;
+    }
+    if (lowest_nwp.fScore > nwp.fScore) {
+      lowest_nwp = nwp;
+    }
+  }
+  return lowest_nwp;
+}
+
+function changeNode<Node>(camefrom: NodeWithPath<Node>[], _key: Node, _value: NodeWithPath<Node>) {
+  for(let nwp of camefrom) {
+    if (nwp.my_node == _key) {
+      nwp.my_parent_node = _value;
+      break;
+    }
+  }
+}
+/*
+function changeNode(camefrom: Edge<Node>[], key: Node, value: Node) {
+  for(var edge in camefrom) {
+  //  if (edge.from === _key) {
+      console.log(edge);
+      //edge.to = _value;
+      break;
+    //}
+  }
+
+}
+*/
