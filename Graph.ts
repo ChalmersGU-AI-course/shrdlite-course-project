5c9dcb94bacabcef1d36332980759998bf759456
Mazdak Farrokhzad <twingoow@gmail.com>
2016-04-30 16:37:20 +0200

    Graph.ts: fixed indentation

--- Graph.ts
+++ Graph.ts
@@ -56,64 +56,68 @@ function aStarSearch<Node> (
     timeout : number
 ) : SearchResult<Node> {
     // A dummy search result: it just picks the first possible neighbour
-    var result : SearchResult<Node> = {
+    let result : SearchResult<Node> = {
         path: [start],
         cost: 0
     };
 
-    var start_nwp: NodeWithPath<Node> = new NodeWithPath(start);
+    let start_nwp: NodeWithPath<Node> = new NodeWithPath(start);
 
     // The nodes already evaluated
-    var closedSet:NodeWithPath<Node>[] = [];
+    let closedSet:NodeWithPath<Node>[] = [];
 
     // Discovered node still not evaluated
-    var openSet:NodeWithPath<Node>[] = [];
+    let openSet:NodeWithPath<Node>[] = [];
     openSet.push(start_nwp);
 
-    var cameFrom:NodeWithPath<Node>[] = [];
+    let cameFrom:NodeWithPath<Node>[] = [];
 
     start_nwp.gScore = 0;
     //console.log(heuristics);
     start_nwp.fScore = heuristics(start_nwp.my_node);
     //console.log(nwp.fScore);
-    var x = 0;
+    let x = 0;
     while (openSet.length > 0 && x < 4) {
-      //openSet
-      var current = getOpenSetLowestfScore(openSet);
-      if (goal(current.my_node)) {
-        console.log("We found shortest way!");
-        // TODO: We need to resolve the closed path here from this node.
-        break;
-      }
-      // remove nodewitpath from openSet
-      var index = openSet.indexOf(current, 0);
-      if (index > -1) {
-        openSet.splice(index, 1);
-      }
-      closedSet.push(current);
-      console.log(current.my_node);
-      console.log(closedSet.length);
+        //openSet
+        let current = getOpenSetLowestfScore(openSet);
+        if (goal(current.my_node)) {
+            console.log("We found shortest way!");
+            // TODO: We need to resolve the closed path here from this node.
+            break;
+        }
 
-      var neigboors: Edge<Node>[] = graph.outgoingEdges(current.my_node);
-      for (let neighbor_edge of neigboors) {
-        var neighboor_nwp: NodeWithPath<Node> = new NodeWithPath(neighbor_edge.to);
-        console.log("Looking in closed list");
-        if (isNodeInList(graph, closedSet, neighbor_edge.to)) { // TODO: We think the problem is here. We don't detect the nodes in the list.
-          console.log("Node was already visited");
-          continue;
+        // remove nodewitpath from openSet
+        let index = openSet.indexOf(current, 0);
+        if (index > -1) {
+            openSet.splice(index, 1);
         }
-        var tentative_gScore:number = current.gScore + neighbor_edge.cost; // only one step to neighbor
-        console.log("Looking in open list");
-        if (!isNodeInList(graph, openSet, neighbor_edge.to)) {
-          openSet.push(new NodeWithPath(neighbor_edge.to));
-        } else if (tentative_gScore >= getGScoreForNode(openSet, neighbor_edge.to)) {
-          continue;
+        closedSet.push(current);
+        console.log(current.my_node);
+        console.log(closedSet.length);
+
+        let neigboors: Edge<Node>[] = graph.outgoingEdges(current.my_node);
+        for (let neighbor_edge of neigboors) {
+            let neighboor_nwp: NodeWithPath<Node> = new NodeWithPath(neighbor_edge.to);
+
+            console.log("Looking in closed list");
+            if (isNodeInList(graph, closedSet, neighbor_edge.to)) { // TODO: We think the problem is here. We don't detect the nodes in the list.
+                console.log("Node was already visited");
+                continue;
+            }
+        
+            const tentative_gScore:number = current.gScore + neighbor_edge.cost; // only one step to neighbor
+            console.log("Looking in open list");
+            if (!isNodeInList(graph, openSet, neighbor_edge.to)) {
+                openSet.push(new NodeWithPath(neighbor_edge.to));
+            } else if (tentative_gScore >= getGScoreForNode(openSet, neighbor_edge.to)) {
+                continue;
+            }
+            neighboor_nwp.my_parent_node = current;
+            neighboor_nwp.gScore = tentative_gScore;
+            neighboor_nwp.fScore = tentative_gScore + heuristics(neighboor_nwp.my_node);
         }
-        neighboor_nwp.my_parent_node = current;
-        neighboor_nwp.gScore = tentative_gScore;
-        neighboor_nwp.fScore = tentative_gScore + heuristics(neighboor_nwp.my_node);
-      }
-      x++; // Helper
+
+        x++; // Helper
     }
 
     return result;
@@ -122,55 +126,53 @@ function aStarSearch<Node> (
 
 class NodeWithPath<Node> {
   //public my_node: Node;
-  public my_parent_node: NodeWithPath<Node>;
-  public gScore: number = 10000000;
-  public fScore: number = 10000000;
+    public my_parent_node: NodeWithPath<Node>;
+    public gScore: number = 10000000;
+    public fScore: number = 10000000;
 
-  constructor(public my_node : Node) {
-
-  }
+    constructor(public my_node : Node) {}
 }
+
 function getGScoreForNode<Node>(list: NodeWithPath<Node>[], element: Node) {
-  for(let nwp of list) {
-    if(nwp.my_node  == element) {
-        return nwp.gScore;
-    }
-  }
-  return -1;
+    for(let nwp of list)
+        if(nwp.my_node  == element)
+            return nwp.gScore;
+
+    return -1;
 }
 
 function isNodeInList<Node>(graph: Graph<Node>, list: NodeWithPath<Node>[], element: Node) {
-  for(let nwp of list) {
-    //if (nwp.my_node.pos.x  == element.pos.x &&
-    //nwp.my_node.pos.y  == element.pos.y) {
-    if (nwp.my_node == element) { // TODO: We think the problem is here. This does not compare correctly.
-      console.log("Was in list");
-      return true;
+    for(let nwp of list) {
+        //if (nwp.my_node.pos.x  == element.pos.x &&
+        //nwp.my_node.pos.y  == element.pos.y) {
+        if (nwp.my_node == element) { // TODO: We think the problem is here. This does not compare correctly.
+            console.log("Was in list");
+            return true;
+        }
     }
-  }
-  console.log("Was not in list");
-  return false;
+    console.log("Was not in list");
+    return false;
 }
 
 function getOpenSetLowestfScore<Node>(openSet: NodeWithPath<Node>[]): NodeWithPath<Node> {
-  var lowest_nwp: NodeWithPath<Node> = null;
-  for(let nwp of openSet) {
-    if (lowest_nwp == null) {
-      lowest_nwp = nwp;
-      continue;
+    var lowest_nwp: NodeWithPath<Node> = null;
+    for(let nwp of openSet) {
+        if (lowest_nwp == null) {
+            lowest_nwp = nwp;
+            continue;
+        }
+
+        if (lowest_nwp.fScore > nwp.fScore)
+            lowest_nwp = nwp;
     }
-    if (lowest_nwp.fScore > nwp.fScore) {
-      lowest_nwp = nwp;
-    }
-  }
-  return lowest_nwp;
+    return lowest_nwp;
 }
 
 function changeNode<Node>(camefrom: NodeWithPath<Node>[], _key: Node, _value: NodeWithPath<Node>) {
-  for(let nwp of camefrom) {
-    if (nwp.my_node == _key) {
-      nwp.my_parent_node = _value;
-      break;
+    for(let nwp of camefrom) {
+        if (nwp.my_node == _key) {
+            nwp.my_parent_node = _value;
+            break;
+        }
     }
-  }
 }
