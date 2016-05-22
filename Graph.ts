dc1f19f45ae2e823da4b2a1fbffc654eb15fb5f0
mictra <michaeltran_93@hotmail.com>
2016-05-22 15:51:20 +0200

    Now handles timeout correctly

--- Graph.ts
+++ Graph.ts
@@ -4,10 +4,6 @@
 /** Graph module
 *
 *  Types for generic A\* implementation.
-*
-*  *NB.* The only part of this module
-*  that you should change is the `aStarSearch` function. Everything
-*  else should be used as-is.
 */
 
 /** An edge in a graph. */
@@ -34,13 +30,8 @@ class SearchResult<Node> {
 }
 
 /**
-* A\* search implementation, parameterised by a `Node` type. The code
-* here is just a template; you should rewrite this function
-* entirely. In this template, the code produces a dummy search result
-* which just picks the first possible neighbour.
+* A\* search implementation, parameterised by a `Node` type.
 *
-* Note that you should not change the API (type) of this function,
-* only its body.
 * @param graph The graph on which to perform A\* search.
 * @param start The initial node.
 * @param goal A function that returns true when given a goal node. Used to determine if the algorithm has reached the goal.
@@ -56,24 +47,26 @@ function aStarSearch<Node>(
     timeout: number
 ): SearchResult<Node> {
 
+    // Start time used to check timeout and convert the timeout time to ms
+    var startTime: number = Date.now();
+    timeout = timeout * 1000;
 
     var result: SearchResult<Node> = {
         path: [],
         cost: 0
     };
 
-    var timer = setTimeout(function(){
-      throw new Error("Something went badly wrong!");
-    }, 1);
     // The set containing nodes that have been evaluated (i.e dequeued from the priority queue)
     var closedNodesSet: collections.Set<Node> = new collections.Set<Node>();
+
     // Set up a dictionary keeping track of minimum gScore for each node discovered
     var gScoreDict: collections.Dictionary<Node, number> = new collections.Dictionary<Node, number>();
     gScoreDict.setValue(start, 0);
+
     // A dictionary keeping track of the parent/predecessor node yielding the best (minimum) score for each (possible) node
     var parentDict: collections.Dictionary<Node, Node> = new collections.Dictionary<Node, Node>();
-    // Compare function used by the PriorityQueue to determine the priority for each node in the queue,
-    // the lesser the score the higher the priority
+
+    // Compare function used by the PriorityQueue to determine the priority for each node in the queue
     function compareNodes(a: Node, b: Node): number {
         // Using the fScore (total score) for comparison
         var aScore: number = gScoreDict.getValue(a) + heuristics(a);
@@ -85,12 +78,18 @@ function aStarSearch<Node>(
         }
         return 0;
     }
+
     // A priority queue containing discovered Nodes still to be evaluated
     var openNodesPQ: collections.PriorityQueue<Node> = new collections.PriorityQueue<Node>(compareNodes);
     openNodesPQ.enqueue(start);
     var currentNode: Node = start;
 
     while (!openNodesPQ.isEmpty()) {
+        if (Date.now() - startTime > timeout) {
+            console.log("Timeout for aStarSearch surpassed. Maximum time: " + timeout + " ms");
+            throw Error("Timeout surpassed error!");
+        }
+
         currentNode = openNodesPQ.dequeue();
         if (goal(currentNode)) {
             break;
@@ -102,33 +101,36 @@ function aStarSearch<Node>(
         for (var j = 0; j < edges.length; j++) {
             // The neighbor node
             var toNode = edges[j].to;
-            // Only continue to calculate the gScore for the neighbor node if the neighbor node hasn't been evaluated (dequeued from the PQ)
+
+            // Only continue to calculate the gScore for the neighbor node if the neighbor node hasn't been evaluated
             if (!closedNodesSet.contains(toNode)) {
                 // Distance from start node to current neighbor node
                 var gScore: number = gScoreDict.getValue(currentNode) + edges[j].cost;
-                // Record/Store the gScore from start to the neighbor node if it hasn't been recorded in the dictionary,
-                // or if the current gScore is better (less) than the already recorded gScore
-                if(collections.isUndefined(gScoreDict.getValue(toNode))){
-                  gScoreDict.setValue(toNode, gScore);
-                  parentDict.setValue(toNode, currentNode);
-                  openNodesPQ.enqueue(toNode);
+
+                // Record/Store the best gScore from start to the neighbor node in the dictionary
+                if (collections.isUndefined(gScoreDict.getValue(toNode))) {
+                    gScoreDict.setValue(toNode, gScore);
+                    parentDict.setValue(toNode, currentNode);
+                    openNodesPQ.enqueue(toNode);
                 } else if (gScore < gScoreDict.getValue(toNode)) {
                     gScoreDict.setValue(toNode, gScore);
                     parentDict.setValue(toNode, currentNode);
                 }
             }
+
         }
 
     }
 
     // Get the optimal score from start to goal node
     result.cost = gScoreDict.getValue(currentNode);
+
     // Construct the path (from goal, back to start and reverse it)
     while (parentDict.containsKey(currentNode)) {
         result.path.push(currentNode);
         currentNode = parentDict.getValue(currentNode);
     }
+
     result.path.reverse();
-    clearTimeout(timer);
     return result;
 }
